efcaeb69343c476d042af1e47db5b03c3d70af52
Alex Todorov <alex.todorov@gluwa.com>
2023-10-30 14:57:44 +0200

    Bump version to 3.4.1 b/c this repo is designated Creditcoin 3
    so all versions should start with "3"!

--- runtime/src/version.rs
+++ runtime/src/version.rs
@@ -6,7 +6,7 @@ use sp_version::RuntimeVersion;
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("frontier-template"),
     impl_name: create_runtime_str!("frontier-template"),
-    authoring_version: 1,
+    authoring_version: 3,
     spec_version: 4,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
