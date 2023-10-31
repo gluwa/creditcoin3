49809c9a3e3e35ee6f3a32b4318338c45951a8fa
Pablo Labarta <pablitolabarta@gmail.com>
2023-10-31 12:22:03 -0300

    rm: dist folder

--- cli/dist/lib/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MICROUNITS_PER_CTC = void 0;
-exports.MICROUNITS_PER_CTC = 1000000000000000000;
--- cli/dist/lib/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getErrorMessage = void 0;
-function getErrorMessage(error) {
-    if (error instanceof Error)
-        return error.message;
-    return String(error);
-}
-exports.getErrorMessage = getErrorMessage;
