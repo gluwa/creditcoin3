57236ab037f085fbbcd07a48248fed25e18585a2
Pablo Labarta <pablitolabarta@gmail.com>
2023-11-01 11:33:41 -0300

    fix: move rotate and set keys to commands

---
+++ cli/src/commands/staking/setKeys.ts
@@ -0,0 +1,49 @@
+import { Command, OptionValues } from 'commander';
+import { newApi } from '../../api';
+import { initStashKeyring } from '../account/keyring';
+import { parseHexStringOrExit } from '../parsing';
+import { requireEnoughFundsToSend, signSendAndWatch } from '../tx';
+
+export function makeSetKeysCommand() {
+    const cmd = new Command('set-keys');
+    cmd.description('Set session keys for a stash account');
+    cmd.option('-k, --keys [keys]', 'Specify keys to set');
+    cmd.option('-r, --rotate', 'Rotate and set new keys');
+
+    cmd.action(setKeysAction);
+    return cmd;
+}
+
+async function setKeysAction(options: OptionValues) {
+    const { api } = await newApi(options.url);
+
+    // Build account
+    const stash = await initStashKeyring(options);
+
+    let keys;
+    if (!options.keys && !options.rotate) {
+        console.log(
+            'Must specify keys to set or generate new ones using the --rotate flag'
+        );
+        process.exit(1);
+    } else if (options.keys && options.rotate) {
+        console.error(
+            'Must either specify keys or rotate to generate new ones, can not do both'
+        );
+        process.exit(1);
+    } else if (options.rotate) {
+        keys = (await api.rpc.author.rotateKeys()).toString();
+    } else {
+        keys = parseHexStringOrExit(options.keys);
+    }
+
+    const tx = api.tx.session.setKeys(keys, '');
+
+    await requireEnoughFundsToSend(tx, stash.address, api);
+
+    const result = await signSendAndWatch(tx, api, stash);
+
+    console.log(result.info);
+
+    process.exit(0);
+}
