a3fc237fee387c5f9a40572316b2ab37c1095978
Pablo Labarta <pablitolabarta@gmail.com>
2023-11-02 18:11:43 -0300

    fix: showAddress test

--- cli/src/test/integration-tests/showAddress.test.ts
+++ cli/src/test/integration-tests/showAddress.test.ts
@@ -1,31 +1,21 @@
-import execa from "execa";
-import { cryptoWaitReady } from "@polkadot/util-crypto";
-import { randomTestAccount } from "./helpers";
+import { execaCommandSync } from 'execa';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { randomTestAccount } from './helpers';
 
-describe("Show address command", () => {
-  it.each([
-    ["using a seed phrase", false],
-    ["using an ecdsa pk", true],
-  ])(
-    "should return the correct address when %s",
-    async (text, ecdsa) => {
-      await cryptoWaitReady();
+describe('Show address command', () => {
+    it('should return the correct address when %s', async () => {
+        await cryptoWaitReady();
 
-      const caller = randomTestAccount(ecdsa);
+        const caller = randomTestAccount();
 
-      const result = execa.commandSync(
-        `node dist/index.js show-address ${ecdsa ? "--ecdsa" : ""}`,
-        {
-          env: {
-            CC_SECRET: caller.secret,
-          },
-        },
-      );
+        const result = execaCommandSync(`node dist/index.js show-address`, {
+            env: {
+                CC_SECRET: caller.secret,
+            },
+        });
 
-      expect(result.stdout.split("Account address: ")[1]).toEqual(
-        caller.address.toString(),
-      );
-    },
-    60000,
-  );
+        expect(result.stdout.split('Account address: ')[1]).toEqual(
+            caller.address.toString()
+        );
+    }, 60000);
 });
