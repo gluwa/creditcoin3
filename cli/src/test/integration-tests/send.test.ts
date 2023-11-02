dcd502c5b16a8fc9a0aa508a71a249a34a5504a4
Pablo Labarta <pablitolabarta@gmail.com>
2023-11-02 18:13:18 -0300

    fix: send command unit test

--- cli/src/test/integration-tests/send.test.ts
+++ cli/src/test/integration-tests/send.test.ts
@@ -1,40 +1,34 @@
-import { newApi } from "../../api";
-import { initKeyringPair } from "../../utils/account";
-import { parseAmountInternal } from "../../utils/parsing";
-import { signSendAndWatch } from "../../utils/tx";
-import { fundAddressesFromSudo, randomTestAccount } from "./helpers";
-import execa from "execa";
+import { execaCommandSync } from 'execa';
+import { newApi } from '../../api';
+import { parseAmountInternal } from '../../lib/parsing';
+import { signSendAndWatch } from '../../lib/tx';
+import {
+    randomTestAccount,
+    fundAddressesFromSudo,
+    initAlithKeyring,
+} from './helpers';
 
-describe("Send command", () => {
-  it.each([
-    ["using a seed phrase", false],
-    ["using an ecdsa pk", true],
-  ])(
-    "should be able to send CTC when %s",
-    async (text, ecdsa) => {
-      const { api } = await newApi();
+describe('Send command', () => {
+    it('should be able to send CTC when %s', async () => {
+        const { api } = await newApi();
 
-      const caller = randomTestAccount(ecdsa);
+        const caller = randomTestAccount();
 
-      const fundTx = await fundAddressesFromSudo(
-        [caller.address],
-        parseAmountInternal("10000"),
-      );
-      await signSendAndWatch(fundTx, api, initKeyringPair("//Alice"));
+        const fundTx = await fundAddressesFromSudo(
+            [caller.address],
+            parseAmountInternal('10000')
+        );
+        await signSendAndWatch(fundTx, api, initAlithKeyring());
 
-      const result = execa.commandSync(
-        `node dist/index.js send --to 5HDRB6edmWwwh6aCDKrRSbisV8iFHdP7jDy18U2mt9w2wEkq --amount 10 ${
-          ecdsa ? "--ecdsa" : ""
-        }`,
-        {
-          env: {
-            CC_SECRET: caller.secret,
-          },
-        },
-      );
+        const result = execaCommandSync(
+            `node dist/index.js send --to 5HDRB6edmWwwh6aCDKrRSbisV8iFHdP7jDy18U2mt9w2wEkq --amount 10`,
+            {
+                env: {
+                    CC_SECRET: caller.secret,
+                },
+            }
+        );
 
-      expect(result.stdout).toContain("Transaction included");
-    },
-    60000,
-  );
+        expect(result.stdout).toContain('Transaction included');
+    }, 60000);
 });
