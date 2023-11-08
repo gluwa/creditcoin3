96bd0394a8ccb15b766877865cdb5643a8fa7b88
Pablo Labarta <pablitolabarta@gmail.com>
2023-11-08 15:42:12 -0300

    rm: old commented fields in api

--- cli/src/lib/types.ts
+++ cli/src/lib/types.ts
@@ -2,6 +2,4 @@ import { ApiPromise } from '@polkadot/api';
 
 export interface CreditcoinApi {
     api: ApiPromise;
-    // extrinsics: Extrinsics;
-    // utils: { signAccountId: (signer: Wallet, accountId: AccountId) => string };
 }
