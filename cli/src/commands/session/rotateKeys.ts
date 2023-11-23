47e2c37fc5b42b47cbdf83d6b1638d5d3a037221
Pablo Labarta <pablitolabarta@gmail.com>
2023-11-23 12:03:25 -0300

    add: better description for rotate-keys command

--- cli/src/commands/session/rotateKeys.ts
+++ cli/src/commands/session/rotateKeys.ts
@@ -3,7 +3,7 @@ import { newApi } from '../../lib';
 
 export function makeRotateKeysCommand() {
     const cmd = new Command('rotate-keys');
-    cmd.description('Rotate session keys for a specified node');
+    cmd.description(`Rotate session keys for a specified node. While it does not require an account, it does require access to the node's unsafe RPCs, either by enabling external calls or by running this CLI tool in the same machine as the node.`);
     cmd.action(rotateKeysAction);
     return cmd;
 }
