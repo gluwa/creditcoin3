5a6f897c36fc3861988ecd003642adeb884d4404
Alex Todorov <alex.todorov@gluwa.com>
2023-12-01 15:11:24 +0200

    Use expectNoDispatchError() and expectNoEventError() in `runtimeUpgrade`
    these are helper functions and provide additional safeguards against
    when something is failing.
    
    Also print the block hash at which the transaction is included!

--- cli/src/lib/api.ts
+++ cli/src/lib/api.ts
@@ -1,5 +1,7 @@
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 import { ApiPromise, WsProvider } from '@polkadot/api';
+import { DispatchError, DispatchResult, EventRecord } from '@polkadot/types/interfaces';
+
 export interface CreditcoinApi {
     api: ApiPromise;
 }
@@ -17,3 +19,33 @@ export async function newApi(url = 'ws://127.0.0.1:9944') {
     await cryptoWaitReady();
     return ccApi;
 }
+
+// helper functions for transactions subscriptions
+const isDispatchError = (instance: any): instance is DispatchResult => {
+    return (instance as DispatchResult) !== undefined;
+};
+
+export const expectNoEventError = (api: ApiPromise, eventRecord: EventRecord) => {
+    const {
+        event: { data },
+    } = eventRecord;
+    if (data[0] && isDispatchError(data[0])) {
+        const dispatchResult = data[0];
+        if (dispatchResult.isErr) {
+            expectNoDispatchError(api, dispatchResult.asErr);
+        }
+    }
+};
+
+const parseModuleError = (api: ApiPromise, dispatchError: DispatchError): string => {
+    const decoded = api.registry.findMetaError(dispatchError.asModule);
+    const { docs, name, section } = decoded;
+    return `${section}.${name}: ${docs.join(' ')}`;
+};
+
+export const expectNoDispatchError = (api: ApiPromise, dispatchError?: DispatchError): void => {
+    if (dispatchError) {
+        const errString = dispatchError.isModule ? parseModuleError(api, dispatchError) : dispatchError.toString();
+        throw new Error(errString);
+    }
+};
