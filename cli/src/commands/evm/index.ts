bdfe9cc985f06c643d521731b38fe88ea6058d86
Pablo Labarta <pablitolabarta@gmail.com>
2023-12-04 15:42:42 -0300

    add: withdraw subcommand to evm command

---
+++ cli/src/commands/evm/index.ts
@@ -0,0 +1,16 @@
+import { Command } from 'commander';
+import { makeEvmFundCommand } from './fund';
+import { makeEvmWithdrawCommand } from './withdraw';
+
+export function makeEvmCommand() {
+    const cmd = new Command('evm');
+    cmd.description('Interact with the EVM side of Creditcoin3');
+    cmd.addCommand(makeEvmFundCommand());
+    cmd.addCommand(makeEvmWithdrawCommand());
+
+    cmd.commands.forEach((command) => {
+        command.option('--no-input', 'Disable interactive prompts');
+        command.option('--evm-url [evm-url]', 'URL of the EVM RPC endpoint to connect to', 'http://127.0.0.1:9944');
+    });
+    return cmd;
+}
---
+++ cli/src/commands/evm/withdraw.ts
@@ -0,0 +1,52 @@
+import { Command, OptionValues } from 'commander';
+import { newApi } from '../../lib';
+import { parseAmountOrExit, requiredInput } from '../../lib/parsing';
+import { initCallerKeyring } from '../../lib/account/keyring';
+
+import { substrateAddressToEvmAddress } from '../../lib/evm/address';
+import { JsonRpcProvider } from 'ethers';
+import { requireEnoughFundsToSend, signSendAndWatch } from '../../lib/tx';
+
+export function makeEvmWithdrawCommand() {
+    const cmd = new Command('withdraw');
+    cmd.description('Withdraw all funds from an associated EVM account to the owned Subtrate one');
+    cmd.option('--show-address', 'Show the associated EVM address and exit');
+    cmd.action(evmWithdrawAction);
+    return cmd;
+}
+
+async function evmWithdrawAction(options: OptionValues) {
+    const { api } = await newApi(options.url as string);
+
+    if (options.showAddress) {
+        const caller = await initCallerKeyring(options);
+        const evmAddress = substrateAddressToEvmAddress(caller.address);
+        console.log(`Associated EVM address: ${evmAddress}. Send funds to it before trying to withdraw.`);
+        process.exit(0);
+    }
+
+    const caller = await initCallerKeyring(options);
+    const evmAddress = substrateAddressToEvmAddress(caller.address);
+    console.log(
+        `Withdrawing all funds from associated EVM address ${evmAddress} into Substrate account ${caller.address}`,
+    );
+
+    const provider = new JsonRpcProvider(options.evmUrl as string);
+    const balance = await provider.getBalance(evmAddress);
+    console.log(balance);
+
+    const tx = api.tx.evm.withdraw(evmAddress, balance.toString());
+
+    await requireEnoughFundsToSend(tx, caller.address, api);
+    const result = await signSendAndWatch(tx, api, caller);
+    console.log(result.info);
+    process.exit(0);
+}
+
+function parseOptions(options: OptionValues) {
+    const amount = parseAmountOrExit(requiredInput(options.amount, 'Failed to send CTC: Must specify an amount'));
+
+    const recipient = options.to as string;
+
+    return { amount, recipient };
+}
