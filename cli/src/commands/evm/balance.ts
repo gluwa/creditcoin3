53e100af77f5a8e1e76b076e6dcd84095d677cb7
Pablo Labarta <pablitolabarta@gmail.com>
2023-12-08 18:39:22 -0300

    add: revamp evm balance util & cmd

--- cli/src/commands/evm/balance.ts
+++ cli/src/commands/evm/balance.ts
@@ -3,12 +3,13 @@ import { parseEVMAddressOrExit } from '../../lib/parsing';
 import { getEvmUrl } from '../../lib/evm/rpc';
 import { toCTCString } from '../../lib/balance';
 import { BN } from '../../lib';
-import { getEVMBalanceOf } from '../../lib/evm/balance';
+import { getEVMBalanceOf, logEVMBalance } from '../../lib/evm/balance';
 
 export function makeEvmBalanceCommand() {
     const cmd = new Command('balance');
     cmd.description('Show balance of an EVM accoun');
     cmd.argument('<address>', 'Address to check balance of');
+    cmd.option('--json', 'Output as JSON');
     cmd.action(evmBalanceAction);
     return cmd;
 }
@@ -17,7 +18,7 @@ async function evmBalanceAction(address: string, options: OptionValues) {
     const balance = await getEVMBalanceOf(
         parseEVMAddressOrExit(address), getEvmUrl(options)
     );
-    const humanBalance = toCTCString(new BN(balance.toString()), 2);
-    console.log('Account balance on EVM: ' + humanBalance.toString());
+
+    logEVMBalance(balance, !options.json);
     process.exit(0);
 }
--- cli/src/lib/evm/balance.ts
+++ cli/src/lib/evm/balance.ts
@@ -1,10 +1,64 @@
 import { JsonRpcProvider } from 'ethers';
+import { toCTCString } from '../balance';
+import Table from 'cli-table3';
+import { BN } from '..';
 
-export async function getEVMBalanceOf(address: string, rpcUrl: string): Promise<bigint> {
+interface EVMBalance
+{
+    address: string;
+    ctc: bigint;
+}
+
+export async function getEVMBalanceOf(address: string, rpcUrl: string): Promise<EVMBalance> {
     // Create an ethers provider and get balance of address
     // Return balance as a bigint
     // NOTE: Seems like the EVM side cannot access the existential deposit amount
     const provider = new JsonRpcProvider(rpcUrl);
     const balance = await provider.getBalance(address);
-    return balance;
+    return { address, ctc: balance } as EVMBalance;
+}
+
+
+
+export async function getTransferFeeEstimation(rpcUrl: string): Promise<bigint> {
+    const provider = new JsonRpcProvider(rpcUrl);
+    const data = await provider.getFeeData();
+    const fee = data.maxFeePerGas;
+    const priority = data.maxPriorityFeePerGas;
+    if (!fee || !priority) {
+        throw new Error('Could not get fee data from RPC at ' + rpcUrl);
+    }
+    const base = fee - priority;
+    const maxFeePerGas = base * BigInt(2) + priority;
+    const feesInCredo = BigInt(21000) * maxFeePerGas; // 21000 is the expected gas limit for a native currency transfer
+    return feesInCredo;
+}
+
+export function logEVMBalance(balance: EVMBalance, human = true) {
+    if (human) {
+        printEVMBalance(balance);
+    } else {
+        printEVMJsonBalance(balance);
+    }
+}
+
+export function printEVMBalance(balance: EVMBalance) {
+    const table = new Table({});
+
+    table.push(
+        ['CTC Balance', toCTCString(new BN(balance.ctc.toString()), 4)],
+    );
+
+    console.log(`Address: ${balance.address}`);
+    console.log(table.toString());
+}
+
+export function printEVMJsonBalance(balance: EVMBalance) {
+    const jsonBalance = {
+        balance: {
+            address: balance.address,
+            ctc: balance.ctc.toString(),
+        },
+    };
+    console.log(JSON.stringify(jsonBalance, null, 2));
 }
