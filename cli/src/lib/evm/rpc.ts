3a17015de19511e1fd3e6cf9b18bb6d24bc9dfbe
Pablo Labarta <pablitolabarta@gmail.com>
2023-12-08 19:37:42 -0300

    refactor: extract inner ws->http convertion

--- cli/src/lib/evm/rpc.ts
+++ cli/src/lib/evm/rpc.ts
@@ -3,7 +3,16 @@ import { OptionValues } from 'commander';
 export function getEvmUrl(options: OptionValues): string {
     const url = options.url as string;
 
-    // Check if it is ws or wss and replace with http or https accordingly
+    if (!url) {
+        throw new Error('EVM URL is required');
+    }
+
+    const httpUrl = convertWsToHttp(url);
+
+    return httpUrl;
+}
+
+export function convertWsToHttp(url: string): string {
     let httpUrl = url;
     if (url.startsWith('ws://')) {
         httpUrl = url.replace('ws://', 'http://');
