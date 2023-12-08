e5a4ea78c2c0dbfbced0176834bdac50ef5f9c84
Pablo Labarta <pablitolabarta@gmail.com>
2023-12-08 19:35:00 -0300

    add: support ws & wss convertion for evm rpc endpoint convertion

--- cli/src/lib/evm/rpc.ts
+++ cli/src/lib/evm/rpc.ts
@@ -2,6 +2,14 @@ import { OptionValues } from 'commander';
 
 export function getEvmUrl(options: OptionValues): string {
     const url = options.url as string;
-    const httpUrl = url.replace('ws://', 'http://');
+
+    // Check if it is ws or wss and replace with http or https accordingly
+    let httpUrl = url;
+    if (url.startsWith('ws://')) {
+        httpUrl = url.replace('ws://', 'http://');
+    } else if (url.startsWith('wss://')) {
+        httpUrl = url.replace('wss://', 'https://');
+    }
+
     return httpUrl;
 }
