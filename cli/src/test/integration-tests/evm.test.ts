39a3854066c0984f86310911446a3959c76e2797
Pablo Labarta <pablitolabarta@gmail.com>
2023-12-08 20:57:35 -0300

    test/add: evm withdraw cmd

--- cli/src/test/integration-tests/evm.test.ts
+++ cli/src/test/integration-tests/evm.test.ts
@@ -6,7 +6,8 @@ import { newApi } from '../../lib';
 import { randomEvmAccount } from './evmHelpers';
 import { getEVMBalanceOf } from '../../lib/evm/balance';
 import { convertWsToHttp } from '../../lib/evm/rpc';
-import { evmAddressToSubstrateAddress } from '../../lib/evm/address';
+import { evmAddressToSubstrateAddress, substrateAddressToEvmAddress } from '../../lib/evm/address';
+import { getBalance } from '../../lib/balance';
 
 describe('EVM commands', () => {
     it('should be able to fund an EVM account', async () => {
@@ -65,6 +66,51 @@ describe('EVM commands', () => {
         // Check that the second account balance is greater than 0
         const evmBalance2 = await getEVMBalanceOf(evmAccount2.address, convertWsToHttp(ALICE_NODE_URL));
         expect(evmBalance2.ctc).toBeGreaterThan(0);
+
+        await api.disconnect();
+    }, 60000);
+
+    it('should be able to withdraw CTC to a Substrate account', async () =>
+    {
+        const { api } = await newApi(ALICE_NODE_URL);
+
+        // Create one EVM account & a Substrate account
+        const evmAccount = randomEvmAccount();
+        const substrateAccount = randomTestAccount();
+
+       // Create and fund the EVM account through its associated Substrate account
+       const substrateAddress = evmAddressToSubstrateAddress(evmAccount.address);
+       const fundTx = await fundAddressesFromSudo([substrateAddress], parseAmountInternal('10000'));
+       await signSendAndWatch(fundTx, api, initAliceKeyring());
+
+        // Fund the Substrate account with 0.1 CTC to pay for fees
+        const fundTx2 = await fundAddressesFromSudo([substrateAccount.address], parseAmountInternal('0.1'));
+        await signSendAndWatch(fundTx2, api, initAliceKeyring());
+
+        // Send 1 CTC from the EVM account to the Substrate account
+        const associatedEvmAccount = substrateAddressToEvmAddress(substrateAccount.address);
+        commandSync(
+            `node ${CLI_PATH} evm send --to ${associatedEvmAccount} --amount 1`,
+            {
+                env: {
+                    EVM_SECRET: evmAccount.mnemonic,
+                },
+            },
+        );
+
+        // Withdraw 1 CTC to the Substrate account
+        commandSync(
+            `node ${CLI_PATH} evm withdraw`,
+            {
+                env: {
+                    CC_SECRET: substrateAccount.secret,
+                },
+            },
+        );
+   
+        // Check that the caller's Substrate account balance is greater than 1
+        const balance = await getBalance(substrateAccount.address, api);
+        expect(BigInt(balance.total.toString())).toBeGreaterThan(1);
 
         await api.disconnect();
     }, 60000);
