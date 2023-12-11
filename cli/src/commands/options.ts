978d0bb6ed77c32fc3b980e66d200a9e312f6f1b
Pablo Labarta <pablitolabarta@gmail.com>
2023-12-11 16:07:27 -0300

    fix: add default url option

--- cli/src/commands/options.ts
+++ cli/src/commands/options.ts
@@ -3,9 +3,9 @@ import { Option } from 'commander';
 // Most used options are URL, NO INPUT, JSON, eCDSA, aDDRESS, AMOUNT, TO, EMV-ADDRESS
 // Create consts for each one using the Option class and export them to be used by other commands
 
-export const urlOption = new Option('-u, --url [url]', 'URL of the node to connect to');
+export const urlOption = new Option('-u, --url [url]', 'URL of the node to connect to').default('ws://127.0.0.1:9944');
 
-export const noInputOption = new Option('-n, --no-input', 'Do not prompt for input');
+export const noInputOption = new Option('--no-input', 'Do not prompt for input');
 
 export const jsonOption = new Option('--json', 'Output as JSON');
 
