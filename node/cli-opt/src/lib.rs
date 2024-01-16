6918776737ae6ea7bfde296831d323dd4b67abc1
Frank Li <frankli.consult@gmail.com>
2024-01-16 12:53:09 -0700

    fix: Remove pallets of txpool from moonbeam

--- node/cli-opt/src/lib.rs
+++ node/cli-opt/src/lib.rs
@@ -20,93 +20,93 @@ pub mod account_key;
 /// Block authoring scheme to be used by the dev service.
 #[derive(Debug, Copy, Clone)]
 pub enum Sealing {
-	/// Author a block immediately upon receiving a transaction into the transaction pool
-	Instant,
-	/// Author a block upon receiving an RPC command
-	Manual,
-	/// Author blocks at a regular interval specified in milliseconds
-	Interval(u64),
+    /// Author a block immediately upon receiving a transaction into the transaction pool
+    Instant,
+    /// Author a block upon receiving an RPC command
+    Manual,
+    /// Author blocks at a regular interval specified in milliseconds
+    Interval(u64),
 }
 
 impl FromStr for Sealing {
-	type Err = String;
+    type Err = String;
 
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		Ok(match s {
-			"instant" => Self::Instant,
-			"manual" => Self::Manual,
-			s => {
-				let millis =
-					u64::from_str_radix(s, 10).map_err(|_| "couldn't decode sealing param")?;
-				Self::Interval(millis)
-			}
-		})
-	}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "instant" => Self::Instant,
+            "manual" => Self::Manual,
+            s => {
+                let millis =
+                    u64::from_str_radix(s, 10).map_err(|_| "couldn't decode sealing param")?;
+                Self::Interval(millis)
+            }
+        })
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum EthApi {
-	Txpool,
-	Debug,
-	Trace,
+    Txpool,
+    Debug,
+    Trace,
 }
 
 impl FromStr for EthApi {
-	type Err = String;
+    type Err = String;
 
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		Ok(match s {
-			"txpool" => Self::Txpool,
-			"debug" => Self::Debug,
-			"trace" => Self::Trace,
-			_ => {
-				return Err(format!(
-					"`{}` is not recognized as a supported Ethereum Api",
-					s
-				))
-			}
-		})
-	}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "txpool" => Self::Txpool,
+            "debug" => Self::Debug,
+            "trace" => Self::Trace,
+            _ => {
+                return Err(format!(
+                    "`{}` is not recognized as a supported Ethereum Api",
+                    s
+                ))
+            }
+        })
+    }
 }
 
 /// Available frontier backend types.
 #[derive(Debug, Copy, Clone, Default, clap::ValueEnum)]
 pub enum FrontierBackendType {
-	/// Either RocksDb or ParityDb as per inherited from the global backend settings.
-	#[default]
-	KeyValue,
-	/// Sql database with custom log indexing.
-	Sql,
+    /// Either RocksDb or ParityDb as per inherited from the global backend settings.
+    #[default]
+    KeyValue,
+    /// Sql database with custom log indexing.
+    Sql,
 }
 
 /// Defines the frontier backend configuration.
 pub enum FrontierBackendConfig {
-	KeyValue,
-	Sql {
-		pool_size: u32,
-		num_ops_timeout: u32,
-		thread_count: u32,
-		cache_size: u64,
-	},
+    KeyValue,
+    Sql {
+        pool_size: u32,
+        num_ops_timeout: u32,
+        thread_count: u32,
+        cache_size: u64,
+    },
 }
 
 impl Default for FrontierBackendConfig {
-	fn default() -> FrontierBackendConfig {
-		FrontierBackendConfig::KeyValue
-	}
+    fn default() -> FrontierBackendConfig {
+        FrontierBackendConfig::KeyValue
+    }
 }
 
 pub struct RpcConfig {
-	pub ethapi: Vec<EthApi>,
-	pub ethapi_max_permits: u32,
-	pub ethapi_trace_max_count: u32,
-	pub ethapi_trace_cache_duration: u64,
-	pub eth_log_block_cache: usize,
-	pub eth_statuses_cache: usize,
-	pub fee_history_limit: u64,
-	pub max_past_logs: u32,
-	pub relay_chain_rpc_urls: Vec<url::Url>,
-	pub tracing_raw_max_memory_usage: usize,
-	pub frontier_backend_config: FrontierBackendConfig,
-	pub no_prometheus_prefix: bool,
+    pub ethapi: Vec<EthApi>,
+    pub ethapi_max_permits: u32,
+    pub ethapi_trace_max_count: u32,
+    pub ethapi_trace_cache_duration: u64,
+    pub eth_log_block_cache: usize,
+    pub eth_statuses_cache: usize,
+    pub fee_history_limit: u64,
+    pub max_past_logs: u32,
+    pub relay_chain_rpc_urls: Vec<url::Url>,
+    pub tracing_raw_max_memory_usage: usize,
+    pub frontier_backend_config: FrontierBackendConfig,
+    pub no_prometheus_prefix: bool,
 }
