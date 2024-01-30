278815fbfcafb3b4430a1dac7308e6f04da0c298
Pablo Labarta <pablitolabarta@gmail.com>
2024-01-30 12:10:49 -0300

    test/add: integration test for wizard+proxy

--- cli/src/test/integration-tests/wizard.test.ts
+++ cli/src/test/integration-tests/wizard.test.ts
@@ -13,7 +13,7 @@ import { getValidatorStatus } from '../../lib/staking/validatorStatus';
 import { newApi } from '../../lib';
 
 describe('integration test: validator wizard setup', () => {
-    it('new validator should appear as waiting after running %s', async () => {
+    it.skip('new validator should appear as waiting after running %s', async () => {
         const { api } = await newApi(ALICE_NODE_URL);
 
         // Fund stash and controller
@@ -31,6 +31,46 @@ describe('integration test: validator wizard setup', () => {
 
         const validatorStatus = await getValidatorStatus(stash.address, api);
 
+        expect(validatorStatus?.waiting).toBe(true);
+        console.log('Validator waiting status is: ', validatorStatus?.waiting);
+
+        await api.disconnect();
+    }, 120000);
+
+    it('new validator should appear as waiting after running wizard with a proxy', async () => {
+        const { api } = await newApi(ALICE_NODE_URL);
+
+        // Fund stash and controller
+        const stash = randomTestAccount();
+        const proxy = randomTestAccount();
+
+        const fundTx = await fundAddressesFromSudo([stash.address, proxy.address], parseAmount('10000'));
+        await signSendAndWatch(fundTx, api, initAliceKeyring());
+
+        // Bond with the stash first (Staking proxies cannot bond from scratch, but can bond-extra)
+        commandSync(`node ${CLI_PATH} bond --amount 1000`, {
+            env: {
+                CC_SECRET: stash.secret,
+            },
+        });
+
+        // Add a staking proxy
+        commandSync(`node ${CLI_PATH} proxy add --proxy ${proxy.address} --type Staking`, {
+            env: {
+                CC_SECRET: stash.secret,
+            },
+        });
+        // Run wizard setup with 1k ctc ang to pair with node Bob
+        commandSync(`node ${CLI_PATH} wizard --url ${BOB_NODE_URL} --use-proxy ${stash.address}`, {
+            env: {
+                CC_PROXY_SECRET: proxy.secret,
+            },
+        });
+
+        const validatorStatus = await getValidatorStatus(stash.address, api);
+
+        expect(validatorStatus?.bonded).toBe(true);
+        expect(validatorStatus?.validating).toBe(true);
         expect(validatorStatus?.waiting).toBe(true);
         console.log('Validator waiting status is: ', validatorStatus?.waiting);
 
