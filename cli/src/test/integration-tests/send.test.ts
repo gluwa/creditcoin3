dc3bd3e2ae1c46b9b844bbe253ab8a9147b4ae5e
Alex Todorov <alex.todorov@gluwa.com>
2024-02-14 17:52:54 +0200

    Add asserts & setup for command failures when proxy isn't the correct one
    - execute tests for all jobs in the matrix
    - modify the proxy settings according to ENV matrix
    - extra asserts in case the command fails

--- cli/src/test/integration-tests/send.test.ts
+++ cli/src/test/integration-tests/send.test.ts
@@ -8,10 +8,22 @@ import {
 } from './helpers';
 import { newApi, ApiPromise, KeyringPair } from '../../lib';
 
+const testDescription = () => {
+    let description = 'should be able to send CTC';
+
+    if (process.env.PROXY_SECRET_VARIANT !== 'valid-proxy') {
+        description = 'should error with not-a-proxy message';
+    } else if (process.env.PROXY_TYPE === 'Staking' || process.env.PROXY_TYPE === 'NonTransfer') {
+        description = `should error with no permission message for proxy type ${process.env.PROXY_TYPE}`;
+    }
+
+    return description;
+};
+
 describe('Send command', () => {
     let api: ApiPromise;
     let caller: any;
-    let proxyAccount: any;
+    let proxy: any;
     let sudoSigner: KeyringPair;
     let CLI: any;
     let nonProxiedCli: any;
@@ -26,20 +38,39 @@ describe('Send command', () => {
     beforeEach(async () => {
         // Create and fund the test and proxy account
         caller = await randomFundedAccount(api, sudoSigner);
-        proxyAccount = await randomFundedAccount(api, sudoSigner);
+        proxy = await randomFundedAccount(api, sudoSigner);
         nonProxiedCli = CLIBuilder({ CC_SECRET: caller.secret });
-        CLI = setUpProxy(nonProxiedCli, caller, proxyAccount);
+        CLI = await setUpProxy(nonProxiedCli, caller, proxy);
     }, 60_000);
 
+    afterEach(() => {
+        tearDownProxy(nonProxiedCli, proxy);
+    });
+
     afterAll(async () => {
-        tearDownProxy(nonProxiedCli, proxyAccount);
         await api.disconnect();
     });
 
-    it('should be able to send CTC', () => {
-        // Send money to Alice
-        const result = CLI(`send --amount 1 --substrate-address 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY`);
-        expect(result.exitCode).toEqual(0);
-        expect(result.stdout).toContain('Transaction included at block');
-    }, 60_000);
+    it(
+        testDescription(),
+        () => {
+            // Send money to Alice
+            try {
+                const result = CLI(`send --amount 1 --substrate-address 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY`);
+                expect(result.exitCode).toEqual(0);
+                expect(result.stdout).toContain('Transaction included at block');
+            } catch (error: any) {
+                expect(error.exitCode).toEqual(1);
+
+                if (process.env.PROXY_SECRET_VARIANT !== 'valid-proxy') {
+                    expect(error.stdout).toContain(`ERROR: ${proxy.address} is not a proxy for ${caller.address}`);
+                } else if (process.env.PROXY_TYPE === 'Staking' || process.env.PROXY_TYPE === 'NonTransfer') {
+                    expect(error.stdout).toContain(
+                        `ERROR: The proxy ${proxy.address} for address ${caller.address} does not have permission to call extrinsics from the balances pallet`,
+                    );
+                }
+            }
+        },
+        60_000,
+    );
 });
