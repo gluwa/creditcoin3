19716950ac84b8a10973767b229bb528db86a260
Alex Todorov <alex.todorov@gluwa.com>
2024-02-16 20:07:51 +0200

    Conditional testing for all other integration tests
    if command doesn't support proxy then skip the test

--- cli/src/test/integration-tests/showAddress.test.ts
+++ cli/src/test/integration-tests/showAddress.test.ts
@@ -1,34 +1,39 @@
 import { commandSync } from 'execa';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 import { CLI_PATH, randomTestAccount } from './helpers';
+import { describeIf } from '../utils';
 import { parseSubstrateAddress, parseEVMAddress } from '../../commands/options';
 import { substrateAddressToEvmAddress } from '../../lib/evm/address';
 
-describe('Show address command', () => {
-    it('should return the correct address', async () => {
-        await cryptoWaitReady();
+describeIf(
+    process.env.PROXY_ENABLED === undefined || process.env.PROXY_ENABLED === 'no',
+    'Show address command',
+    () => {
+        it('should return the correct address', async () => {
+            await cryptoWaitReady();
 
-        const caller = randomTestAccount();
+            const caller = randomTestAccount();
 
-        const result = commandSync(`node ${CLI_PATH} show-address`, {
-            env: {
-                CC_SECRET: caller.secret,
-            },
-        }).stdout;
+            const result = commandSync(`node ${CLI_PATH} show-address`, {
+                env: {
+                    CC_SECRET: caller.secret,
+                },
+            }).stdout;
 
-        const substrateAddress = parseSubstrateAddress(
-            result
-                .split(/\r?\n/)[0] // First line of the output
-                .split('Account Substrate address: ')[1], // Substrate address
-        );
+            const substrateAddress = parseSubstrateAddress(
+                result
+                    .split(/\r?\n/)[0] // First line of the output
+                    .split('Account Substrate address: ')[1], // Substrate address
+            );
 
-        const evmAddress = parseEVMAddress(
-            result
-                .split(/\r?\n/)[1] // Second line of the output
-                .split('Associated EVM address: ')[1], // EVM address
-        );
+            const evmAddress = parseEVMAddress(
+                result
+                    .split(/\r?\n/)[1] // Second line of the output
+                    .split('Associated EVM address: ')[1], // EVM address
+            );
 
-        expect(substrateAddress).toEqual(caller.address.toString());
-        expect(evmAddress).toEqual(substrateAddressToEvmAddress(caller.address));
-    }, 60000);
-});
+            expect(substrateAddress).toEqual(caller.address.toString());
+            expect(evmAddress).toEqual(substrateAddressToEvmAddress(caller.address));
+        }, 60000);
+    },
+);
