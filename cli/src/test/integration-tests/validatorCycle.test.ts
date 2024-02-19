2ad3a2378fbf98cc08f5b0a4591d3ee50512988d
Alex Todorov <alex.todorov@gluwa.com>
2024-02-19 15:52:48 +0200

    Adjust validatorCycle.test.ts to work with proxies

--- cli/src/test/integration-tests/validatorCycle.test.ts
+++ cli/src/test/integration-tests/validatorCycle.test.ts
@@ -1,22 +1,24 @@
 import { mnemonicValidate } from '@polkadot/util-crypto';
-import { commandSync } from 'execa';
-import { BN, newApi } from '../../lib';
+import { BN, newApi, ApiPromise, KeyringPair } from '../../lib';
 import { getBalance, printBalance } from '../../lib/balance';
 import { parseHexStringInternal } from '../../lib/parsing';
 import { getValidatorStatus } from '../../lib/staking/validatorStatus';
 import { signSendAndWatch } from '../../lib/tx';
 import {
-    BOB_NODE_URL,
     ALICE_NODE_URL,
+    BOB_NODE_URL,
     fundFromSudo,
     waitEras,
     initAliceKeyring,
     increaseValidatorCount,
-    CLI_PATH,
+    randomFundedAccount,
+    randomTestAccount,
+    setUpProxy,
+    tearDownProxy,
+    CLIBuilder,
 } from './helpers';
 import { describeIf } from '../utils';
-import { isAddress } from 'ethers';
-import { parseAmount, parseEVMAddress, parseSubstrateAddress } from '../../commands/options';
+import { parseAmount } from '../../commands/options';
 
 describeIf(
     process.env.PROXY_ENABLED === undefined ||
@@ -24,154 +26,143 @@ describeIf(
         (process.env.PROXY_ENABLED === 'yes' && process.env.PROXY_SECRET_VARIANT === 'valid-proxy'),
     'integration test: validator manual setup',
     () => {
-        it('full validator cycle', async () => {
-            // Bob's node is used for checking its configuration as a validator
-            // and for sending extrinsics using the CLI
-            const bobApi = (await newApi(BOB_NODE_URL)).api;
+        let api: ApiPromise;
+        let proxy: any;
+        let caller: any;
+        let sudoSigner: KeyringPair;
+        let CLI: any;
+        let nonProxiedCli: any;
 
-            // While CLI commands always send extrinsics through Bob's node,
-            // sudo calls and state checks both use Alice's node
-            const aliceApi = (await newApi(ALICE_NODE_URL)).api;
+        beforeAll(async () => {
+            ({ api } = await newApi(ALICE_NODE_URL));
 
-            const stashSecret = // Creating two accounts using `new` should return two valid mnemonic seeds
-                commandSync(`node ${CLI_PATH} new`).stdout.split('Seed phrase: ')[1];
+            // Create a reference to sudo for funding accounts
+            sudoSigner = initAliceKeyring();
+        });
 
+        beforeEach(async () => {
+            const stashSecret = CLIBuilder({})('new').stdout.split('Seed phrase: ')[1];
             expect(mnemonicValidate(stashSecret)).toBe(true);
-
             console.log('Stash seed: ', stashSecret);
 
-            // Getting both Substrate and EVM addresses using `show-address` should return two valid addresses
-            const showAddressResult = commandSync(`node ${CLI_PATH} show-address`, {
-                env: {
-                    CC_SECRET: stashSecret,
-                },
-            }).stdout;
-
-            const substrateAddress = parseSubstrateAddress(
-                showAddressResult
-                    .split(/\r?\n/)[0] // First line of the output
-                    .split('Account Substrate address: ')[1], // Substrate address
-            );
-
-            const evmAddress = parseEVMAddress(
-                showAddressResult
-                    .split(/\r?\n/)[1] // Second line of the output
-                    .split('Associated EVM address: ')[1], // EVM address
-            );
-
-            expect(isAddress(evmAddress)).toBe(true);
+            caller = randomTestAccount(stashSecret);
+            nonProxiedCli = CLIBuilder({ CC_SECRET: caller.secret });
 
             // Funding the stash account should make its balance equal to the amount funded
             const fundAmount = parseAmount('10000');
-
-            const fundTx = await fundFromSudo(substrateAddress, fundAmount);
-            await signSendAndWatch(fundTx, aliceApi, initAliceKeyring());
-            const stashBalance = (await getBalance(substrateAddress, aliceApi)).transferable;
+            const fundTx = await fundFromSudo(caller.address, fundAmount);
+            await signSendAndWatch(fundTx, api, initAliceKeyring());
+            const stashBalance = (await getBalance(caller.address, api)).transferable;
             expect(stashBalance.toString()).toBe(fundAmount.toString());
 
+            // configure proxy
+            proxy = await randomFundedAccount(api, sudoSigner);
+            const wrongProxy = await randomFundedAccount(api, sudoSigner);
+            CLI = await setUpProxy(nonProxiedCli, caller, proxy, wrongProxy);
+        }, 120_000);
+
+        afterEach(() => {
+            tearDownProxy(nonProxiedCli, proxy);
+        }, 30_000);
+
+        afterAll(async () => {
+            await api.disconnect();
+        });
+
+        it('full validator cycle', async () => {
             // Bonding 1k ctc from stash
             const bondAmount = '1000';
-            commandSync(`node ${CLI_PATH} bond --amount ${bondAmount} --url ${BOB_NODE_URL}`, {
-                env: {
-                    CC_SECRET: stashSecret,
-                },
-            });
+            let result = nonProxiedCli(`bond --amount ${bondAmount}`);
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
+
             // wait 5 seconds for nodes to sync
             await new Promise((resolve) => setTimeout(resolve, 5000));
-            const stashStatus = await getValidatorStatus(substrateAddress, aliceApi);
+            const stashStatus = await getValidatorStatus(caller.address, api);
             expect(stashStatus?.bonded).toBe(true);
 
-            const stashBondedBalance = (await getBalance(substrateAddress, aliceApi)).bonded;
+            const stashBondedBalance = (await getBalance(caller.address, api)).bonded;
             expect(stashBondedBalance.toString()).toBe(parseAmount(bondAmount).toString());
 
             // Rotating session keys for the node should return a valid hex string
             const newKeys = parseHexStringInternal(
-                commandSync(`node ${CLI_PATH} rotate-keys --url ${BOB_NODE_URL}`).stdout.split('New keys: ')[1],
+                nonProxiedCli(`rotate-keys --url ${BOB_NODE_URL}`).stdout.split('New keys: ')[1],
             );
 
             // Setting session keys for the controller should
             // - make the validator (stash) next session keys equal to the new keys
             // - make the new keys appear as the node's session keys
-            commandSync(`node ${CLI_PATH} set-keys --keys ${newKeys} --url ${BOB_NODE_URL}`, {
-                env: {
-                    CC_SECRET: stashSecret,
-                },
-            });
+            result = CLI(`set-keys --keys ${newKeys} --url ${BOB_NODE_URL}`);
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
+
             // wait 5 seconds for nodes to sync
             await new Promise((resolve) => setTimeout(resolve, 5000));
-            const validatorSessionKeys = await aliceApi.query.session.nextKeys(substrateAddress);
+            const validatorSessionKeys = await api.query.session.nextKeys(caller.address);
             expect(validatorSessionKeys.toHex()).toBe(newKeys);
+            const bobApi = (await newApi(BOB_NODE_URL)).api;
             const nodeHasKeys = (await bobApi.rpc.author.hasSessionKeys(newKeys)).isTrue;
             expect(nodeHasKeys).toBe(true);
+            await bobApi.disconnect();
 
             // Signaling intention to validate should make the validator (stash) appear as waiting
-            commandSync(`node ${CLI_PATH} validate --commission 1 --url ${BOB_NODE_URL}`, {
-                env: {
-                    CC_SECRET: stashSecret,
-                },
-            });
+            result = CLI('validate --commission 1');
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
 
-            const stashStatusAfterValidating = await getValidatorStatus(substrateAddress, bobApi);
+            const stashStatusAfterValidating = await getValidatorStatus(caller.address, api);
             expect(stashStatusAfterValidating?.waiting).toBe(true);
 
             // After increasing the validator count, (forcing an era- currently not) and waiting for the next era,
             // the validator should become elected & active.
-            await increaseValidatorCount(aliceApi, initAliceKeyring(), 2);
-            await waitEras(2, aliceApi);
-            const stashStatusAfterEra = await getValidatorStatus(substrateAddress, bobApi);
+            await increaseValidatorCount(api, initAliceKeyring(), 2);
+            await waitEras(2, api);
+            const stashStatusAfterEra = await getValidatorStatus(caller.address, api);
             expect(stashStatusAfterEra?.active).toBe(true);
 
             // After waiting for another era, the validator should have accumulated era rewards to distribute
-            const startingEra = (await aliceApi.derive.session.info()).activeEra.toNumber();
+            const startingEra = (await api.derive.session.info()).activeEra.toNumber();
             console.log('Starting era: ', startingEra);
-            await waitEras(1, aliceApi);
+            await waitEras(1, api);
 
             // After distributing rewards, the validator staked balance should increase
             // (because it was set to staked)
-            const balanceBeforeRewards = await getBalance(substrateAddress, aliceApi);
+            const balanceBeforeRewards = await getBalance(caller.address, api);
             console.log(balanceBeforeRewards.bonded.toString());
 
-            commandSync(
-                `node ${CLI_PATH} distribute-rewards --url ${BOB_NODE_URL} --substrate-address ${substrateAddress} --era ${startingEra}`,
-                {
-                    env: {
-                        CC_SECRET: stashSecret,
-                    },
-                },
-            );
+            result = CLI(`distribute-rewards --substrate-address ${caller.address} --era ${startingEra}`);
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
 
             // wait 5 seconds for nodes to sync
             await new Promise((resolve) => setTimeout(resolve, 5000));
-            const balanceAfterRewards = await getBalance(substrateAddress, aliceApi);
+            const balanceAfterRewards = await getBalance(caller.address, api);
             console.log(balanceAfterRewards.bonded.toString());
             const balanceIncreased = balanceAfterRewards.bonded.gt(balanceBeforeRewards.bonded);
             expect(balanceIncreased).toBe(true);
 
             // After executing the chill commmand, the validator should no longer be active nor waiting
-            commandSync(`node ${CLI_PATH} chill --url ${BOB_NODE_URL}`, {
-                env: {
-                    CC_SECRET: stashSecret,
-                },
-            });
+            result = CLI('chill');
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
+
             // wait 5 seconds for nodes to sync
-            await waitEras(2, aliceApi);
-            const stashStatusAfterChill = await getValidatorStatus(substrateAddress, bobApi);
+            await waitEras(2, api);
+            const stashStatusAfterChill = await getValidatorStatus(caller.address, api);
             expect(stashStatusAfterChill?.active).toBe(false);
             expect(stashStatusAfterChill?.waiting).toBe(false);
 
             // After unbonding, the validator should no longer be bonded
-            commandSync(
+            result = CLI(
                 // Unbonding defaults to max if it exceeds the bonded amount
-                `node ${CLI_PATH} unbond --url ${BOB_NODE_URL} --amount 100000`,
-                {
-                    env: {
-                        CC_SECRET: stashSecret,
-                    },
-                },
+                'unbond --amount 100000',
             );
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
+
             // wait 5 seconds for nodes to sync
             await new Promise((resolve) => setTimeout(resolve, 5000));
-            const balanceAfterUnbonding = await getBalance(substrateAddress, aliceApi);
+            const balanceAfterUnbonding = await getBalance(caller.address, api);
             const isUnbonding = balanceAfterUnbonding.unbonding.gt(new BN(0));
             printBalance(balanceAfterRewards);
             printBalance(balanceAfterUnbonding);
@@ -181,25 +172,20 @@ describeIf(
 
             // After unbonding and waiting for the unbonding period, the validator should be able to withdraw
             // the unbonded amount and end up with more funds than the initial funding
-            const unbondingPeriod = aliceApi.consts.staking.bondingDuration.toNumber();
+            const unbondingPeriod: number = api.consts.staking.bondingDuration.toNumber();
             console.log('Unbonding period: ', unbondingPeriod);
-            await waitEras(unbondingPeriod + 1, aliceApi, true);
+            await waitEras(unbondingPeriod + 1, api, true);
 
-            commandSync(`node ${CLI_PATH} withdraw-unbonded --url ${BOB_NODE_URL}`, {
-                env: {
-                    CC_SECRET: stashSecret,
-                },
-            });
+            result = CLI('withdraw-unbonded');
+            expect(result.exitCode).toEqual(0);
+            expect(result.stdout).toContain('Transaction included at block');
 
             // wait 5 seconds for nodes to sync
             await new Promise((resolve) => setTimeout(resolve, 5000));
-            const balanceAfterWithdraw = await getBalance(substrateAddress, aliceApi);
+            const balanceAfterWithdraw = await getBalance(caller.address, api);
             printBalance(balanceAfterWithdraw);
-            const stashAmount = fundAmount;
+            const stashAmount = parseAmount('10000');
             expect(balanceAfterWithdraw.transferable.gte(stashAmount)).toBe(true);
-
-            await aliceApi.disconnect();
-            await bobApi.disconnect();
-        }, 2000000);
+        }, 2_000_000);
     },
 );
