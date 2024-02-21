9504154240e48c201d70bc821a1d2765d20ab760
Alex Todorov <alex.todorov@gluwa.com>
2024-02-21 20:27:50 +0200

    Stricter control over git tag name suffixes
    to avoid the situation where a release is tagged as x.y.z-testnet but
    the release artifacts are actually built from the `dev` branch!

---
+++ .github/check-tag-suffix-vs-origin-branch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GIT_TAG=$(git describe --tag)
+SUFFIX_FROM_GIT_TAG=$(echo "$GIT_TAG" | cut -d"-" -f2,99)
+NEAREST_GIT_BRANCH=$(git branch --contains | grep "\* " | cut -f2 -d" ")
+
+echo "INFO: git tag: '$GIT_TAG'"
+echo "INFO: suffix from git tag: '$SUFFIX_FROM_GIT_TAG'"
+echo "INFO: nearest git branch '$NEAREST_GIT_BRANCH'"
+
+if [[ "$SUFFIX_FROM_GIT_TAG" == "devnet" && "$NEAREST_GIT_BRANCH" == "dev" ]]; then
+    echo "PASS: good match for -devnet releases"
+    exit 0
+fi
+
+if [[ "$SUFFIX_FROM_GIT_TAG" == "testnet" && "$NEAREST_GIT_BRANCH" == "testnet" ]]; then
+    echo "PASS: good match for -testnet releases"
+    exit 0
+fi
+
+if [[ "$SUFFIX_FROM_GIT_TAG" == "mainnet" && "$NEAREST_GIT_BRANCH" == "main" ]]; then
+    echo "PASS: good match for -mainnet releases"
+    exit 0
+fi
+
+echo "FAIL: Looks like tag name doesn't match the branch it came from"
+exit 1
