5fea6dd869ce8154961cd19afc15428853414e1e
Pablo Labarta <pablitolabarta@gmail.com>
2024-02-22 17:43:08 -0300

    rm: evm wallet & evm send cmd

--- cli/src/commands/evm/index.ts
+++ cli/src/commands/evm/index.ts
@@ -1,7 +1,6 @@
 import { Command } from 'commander';
 import { makeEvmFundCommand } from './fund';
 import { makeEvmWithdrawCommand } from './withdraw';
-import { makeEvmSendCommand } from './send';
 import { makeEvmBalanceCommand } from './balance';
 import { noInputOption, urlOption } from '../options';
 
@@ -10,7 +9,6 @@ export function makeEvmCommand() {
     cmd.description('Interact with the EVM side of Creditcoin3');
     cmd.addCommand(makeEvmBalanceCommand());
     cmd.addCommand(makeEvmFundCommand());
-    cmd.addCommand(makeEvmSendCommand());
     cmd.addCommand(makeEvmWithdrawCommand());
 
     cmd.commands.forEach((command) => {
