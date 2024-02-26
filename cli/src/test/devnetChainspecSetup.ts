3ff8ae75c8f3e210e32d2db45fbeb3c9a8c01d13
Alex Todorov <alex.todorov@gluwa.com>
2024-02-26 16:03:49 +0200

    CSUB-1054: Update expected values for Devnet
    because it is build with --features "fast-runtime devnet" where:
    
    - block time = 5 sec
    - epoch duration = 2hrs (1440 blocks)

--- cli/src/test/devnetChainspecSetup.ts
+++ cli/src/test/devnetChainspecSetup.ts
@@ -1,9 +1,10 @@
 import { default as globalSetup } from './blockchainSetup';
 
 const setup = () => {
-    (global as any).CREDITCOIN_EXPECTED_EPOCH_DURATION = 2880;
-    (global as any).CREDITCOIN_EXPECTED_BLOCK_TIME = 15000;
-    (global as any).CREDITCOIN_EXPECTED_MINIMUM_PERIOD = 7500;
+    // --features "fast-runtime devnet"
+    (global as any).CREDITCOIN_EXPECTED_EPOCH_DURATION = 1440;
+    (global as any).CREDITCOIN_EXPECTED_BLOCK_TIME = 5000;
+    (global as any).CREDITCOIN_EXPECTED_MINIMUM_PERIOD = 720;
 
     globalSetup();
 };
