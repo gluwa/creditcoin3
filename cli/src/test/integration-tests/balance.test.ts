18b121ce04dec1b642d5384cd98d794d24105cd2
Alex Todorov <alex.todorov@gluwa.com>
2024-03-06 14:30:14 +0200

    Assert that total balance includes associated evm funds

--- cli/src/test/integration-tests/balance.test.ts
+++ cli/src/test/integration-tests/balance.test.ts
@@ -1,17 +1,20 @@
-import { ALICE_NODE_URL, BOB_NODE_URL, randomTestAccount, CLIBuilder } from './helpers';
-import { newApi, ApiPromise } from '../../lib';
+import { ALICE_NODE_URL, BOB_NODE_URL, initAliceKeyring, randomFundedAccount, CLIBuilder } from './helpers';
+import { newApi, ApiPromise, BN, KeyringPair, MICROUNITS_PER_CTC } from '../../lib';
 
 describe('balance', () => {
     let api: ApiPromise;
     let randomAccount: any;
     let CLI: any;
+    let sudoSigner: KeyringPair;
 
     beforeAll(async () => {
         ({ api } = await newApi(ALICE_NODE_URL));
+
+        sudoSigner = initAliceKeyring();
     });
 
-    beforeEach(() => {
-        randomAccount = randomTestAccount();
+    beforeEach(async () => {
+        randomAccount = await randomFundedAccount(api, sudoSigner);
 
         // eslint-disable-next-line @typescript-eslint/naming-convention
         CLI = CLIBuilder({});
@@ -45,21 +48,28 @@ describe('balance', () => {
     }, 30_000);
 
     it('result should be JSON when --json specified', () => {
-        const result = CLI(`balance --json --substrate-address ${randomAccount.address} --url ${BOB_NODE_URL}`);
+        // setup - transfer some funds to the associated EVM account
+        const fiftyCtc = 50 * MICROUNITS_PER_CTC;
+        const cli = CLIBuilder({ CC_SECRET: randomAccount.secret });
+        let result = cli(`evm fund --evm-address ${randomAccount.evmAddress} --amount 50 --url ${BOB_NODE_URL}`);
+        expect(result.exitCode).toEqual(0);
+        expect(result.stdout).toContain('Transaction included');
+
+        // test - grab the balance - non-authenticated
+        result = CLI(`balance --json --substrate-address ${randomAccount.address} --url ${BOB_NODE_URL}`);
         expect(result.exitCode).toEqual(0);
 
         const parsed = JSON.parse(result.stdout);
 
         expect(parsed).toHaveProperty('balance');
         expect(parsed).toHaveProperty('balance.address', randomAccount.address);
-        expect(parsed).toHaveProperty('balance.transferable', '0');
+        expect(parsed).toHaveProperty('balance.transferable');
         expect(parsed).toHaveProperty('balance.bonded', '0');
-        expect(parsed).toHaveProperty('balance.evm', '0');
+        expect(parsed).toHaveProperty('balance.evm', fiftyCtc.toString());
         expect(parsed).toHaveProperty('balance.locked', '0');
         expect(parsed).toHaveProperty('balance.unbonding', '0');
-        expect(parsed).toHaveProperty('balance.total', '0');
 
-        // todo: make sure Total reflects EVM, see CSUB-1061
-        // https://github.com/gluwa/creditcoin3/pull/245
-    }, 30_000);
+        const expected = new BN(parsed.balance.transferable).add(new BN(fiftyCtc.toString()));
+        expect(parsed).toHaveProperty('balance.total', expected.toString());
+    }, 60_000);
 });
