3a9d996639d1bd8573ab7533f3ab8a92e9e26298
dylanVerstraete <dylan_verstraete1@hotmail.com>
2024-03-19 15:49:30 +0100

    chore: add bootnodes to chainspecs
    chore: remove all bootnodes from all specs
    
    feat: add script to check if there are any bootnodes in the chainspecs
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    chore: update script

---
+++ .github/check-bootnodes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Directory containing JSON files
+chainspecs_dir="chainspecs"
+
+# Check if chainspecs directory exists
+if [ ! -d "$chainspecs_dir" ]; then
+    echo "Error: Directory $chainspecs_dir does not exist."
+    exit 1
+fi
+
+# Flag to track if bootNodes are found
+bootnodes_found=0
+
+# Iterate over JSON files in the directory
+for file in "$chainspecs_dir"/*.json; do
+    echo "Checking $file"
+    # Read the JSON file
+    json=$(cat "$file")
+
+    # Check if the bootNodes field is empty
+    if [[ $(echo "$json" | jq -r '.bootNodes | length') -ne 0 ]]; then
+        echo "BootNodes field is not empty."
+        bootnodes_found=1
+    fi
+done
+
+# Exit with error if bootNodes are found
+if [ $bootnodes_found -eq 1 ]; then
+    echo "Error: BootNodes found in at least one chainspec file."
+    exit 1
+else
+    echo "No BootNodes found in any chainspec file."
+    exit 0
+fi
