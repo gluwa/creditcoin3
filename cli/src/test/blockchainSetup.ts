ab9ddeaeb4ca9596834afd9b8a6eff6b6e81578d
Alex Todorov <alex.todorov@gluwa.com>
2024-03-21 13:03:38 +0200

    Halve the number of blocks/epoch for fast-runtime feature
    this should speed up integration tests in CI b/c we'd have to wait less
    time for unbonded funds to be unlocked

--- cli/src/test/blockchainSetup.ts
+++ cli/src/test/blockchainSetup.ts
@@ -68,7 +68,7 @@ const setup = () => {
     }
 
     if ((global as any).CREDITCOIN_EXPECTED_EPOCH_DURATION === undefined) {
-        (global as any).CREDITCOIN_EXPECTED_EPOCH_DURATION = 15;
+        (global as any).CREDITCOIN_EXPECTED_EPOCH_DURATION = 7;
     }
 
     if ((global as any).CREDITCOIN_EXPECTED_BLOCK_TIME === undefined) {
