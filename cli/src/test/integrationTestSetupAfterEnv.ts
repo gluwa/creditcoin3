ac6822403f76a9417e1db59a79c6cfa7723d452b
Alex Todorov <alex.todorov@gluwa.com>
2024-03-27 10:45:14 +0200

    Short-circuit test execution if the chain has stopped finalizing
    instead of blindly pushing forward which causes CI jobs to take hours
    and hours of execution time and clog the pipeline.
    
    Add a global beforeEach() hook which executes *before* any of the
    hooks defined for each test scenario. This should help us detect
    situations where a newly added test messes up the environment and
    makes the chain to not finalize blocks anymore!
    
    Add a helper function to exercise the assertions and make sure we
    disconnect any open API connections.

--- cli/src/test/integrationTestSetupAfterEnv.ts
+++ cli/src/test/integrationTestSetupAfterEnv.ts
@@ -1,4 +1,4 @@
-import { startAliceAndBob, killCreditcoinNodes } from './utils';
+import { expectIsFinalizing, startAliceAndBob, killCreditcoinNodes } from './utils';
 
 global.beforeAll(async () => {
     // alternatively we can duplicate utils.describeIf() and place this
@@ -9,4 +9,9 @@ global.beforeAll(async () => {
 
 global.afterAll(() => {
     killCreditcoinNodes();
+});
+
+global.beforeEach(async () => {
+    // short-circuit test execution if the chain has stopped finalizing
+    await expectIsFinalizing();
 });
