baed53085ec88574666d6c9bfbafa6ac933a0420
Alex Todorov <alex.todorov@gluwa.com>
2024-04-02 19:55:38 +0300

    Add custom message to expect() to tell us in which iteration it failed

--- cli/src/test/integration-tests.config.ts
+++ cli/src/test/integration-tests.config.ts
@@ -1,9 +1,10 @@
+import 'jest-expect-message';
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    setupFilesAfterEnv: ['./integrationTestSetupAfterEnv.ts'],
+    setupFilesAfterEnv: ['jest-expect-message', './integrationTestSetupAfterEnv.ts'],
 };
 
 export default config;
