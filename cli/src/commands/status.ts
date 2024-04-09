7ed64885e57a4b134cfe17e4cffd1d52ba649021
Alex Todorov <alex.todorov@gluwa.com>
2024-04-09 11:56:37 +0300

    Display era and block info in status command
    and remove the --chain option

--- cli/src/commands/status.ts
+++ cli/src/commands/status.ts
@@ -1,14 +1,12 @@
 import { Command, OptionValues } from 'commander';
 import { newApi } from '../lib';
 import { parseBoolean } from '../lib/parsing';
-import { getChainStatus, printChainStatus } from '../lib/chain/status';
 import { getValidatorStatus, printValidatorStatus } from '../lib/staking/validatorStatus';
 import { substrateAddressOption } from './options';
 
 export function makeStatusCommand() {
     const cmd = new Command('status');
     cmd.description('Get staking status for an address');
-    cmd.option('--chain', 'Show chain status');
     cmd.addOption(substrateAddressOption);
     cmd.action(statusAction);
     return cmd;
@@ -18,16 +16,6 @@ async function statusAction(options: OptionValues) {
     const { api } = await newApi(options.url as string);
 
     const showValidatorStatus = parseBoolean(options.substrateAddress);
-    let showChainStatus = parseBoolean(options.chain);
-
-    if (!showValidatorStatus && !showChainStatus) {
-        showChainStatus = true;
-    }
-
-    if (showChainStatus) {
-        const chainStatus = await getChainStatus(api);
-        printChainStatus(chainStatus);
-    }
 
     if (showValidatorStatus) {
         const validatorAddr = options.substrateAddress as string;
