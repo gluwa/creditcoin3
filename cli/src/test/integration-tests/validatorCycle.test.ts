57bcae5d9c2b3629b4380a717a49f2d0e167187b
Alex Todorov <alex.todorov@gluwa.com>
2024-04-10 11:58:56 +0300

    Wait for the unbonding period, but no longer

--- cli/src/test/integration-tests/validatorCycle.test.ts
+++ cli/src/test/integration-tests/validatorCycle.test.ts
@@ -173,7 +173,7 @@ describeIf(
             // the unbonded amount and end up with more funds than the initial funding
             const unbondingPeriod: number = api.consts.staking.bondingDuration.toNumber();
             console.log('Unbonding period: ', unbondingPeriod);
-            await waitEras(unbondingPeriod + 1, api);
+            await waitEras(unbondingPeriod, api);
 
             result = CLI('withdraw-unbonded');
             expect(result.exitCode).toEqual(0);
