3eee75c112c15e572d7cf504fa8b81f240a70182
Alex Todorov <alex.todorov@gluwa.com>
2024-04-24 21:46:21 +0300

    No need for some variables to be globally accessible

--- cli/src/test/blockchain-tests/substrate-transfer.test.ts
+++ cli/src/test/blockchain-tests/substrate-transfer.test.ts
@@ -6,10 +6,8 @@ import { newApi, ApiPromise } from '../../lib';
 
 describe('Precompile: transfer_substrate()', (): void => {
     let contract: any;
-    let amount: bigint;
     let destination: any;
     let destinationBalanceBefore: bigint;
-    let destinationBalanceAfter: bigint;
     let provider: any;
     let alith: any;
     let alithBalanceBefore: bigint;
@@ -43,7 +41,7 @@ describe('Precompile: transfer_substrate()', (): void => {
     });
 
     test('should work when caller has enough funds', async () => {
-        amount = parseEther('10.0');
+        const amount = parseEther('10.0');
         const result = await contract.transfer_substrate(destination.addressRaw, amount, {
             gasPrice,
         });
@@ -51,7 +49,10 @@ describe('Precompile: transfer_substrate()', (): void => {
         const txHash = result?.hash;
 
         const alithBalanceAfter: bigint = await provider.getBalance(alith.address);
-        destinationBalanceAfter = (await api.derive.balances.all(destination.address)).availableBalance.toBigInt();
+        const destinationBalanceAfter = (
+            await api.derive.balances.all(destination.address)
+        ).availableBalance.toBigInt();
+
         expect(txHash).toBeDefined();
         expect(receipt).toBeDefined();
         expect(alithBalanceBefore).toBe(alithBalanceAfter + amount + BigInt(receipt.cumulativeGasUsed * gasPrice));
@@ -61,7 +62,8 @@ describe('Precompile: transfer_substrate()', (): void => {
     test('should fail when sending more than total issuance', async () => {
         // a local development chain starts with total issuance of 14 M CTC
         // trying to send 1 bil
-        amount = parseEther('1000000000.0');
+        const amount = parseEther('1000000000.0');
+
         await expect(
             contract.transfer_substrate(destination.addressRaw, amount, {
                 gasPrice,
@@ -73,7 +75,7 @@ describe('Precompile: transfer_substrate()', (): void => {
 
     test('should fail when sending more than available funds', async () => {
         // Alice starts with 1M CTC, try sending 1.9 mil
-        amount = parseEther('1999989.9');
+        const amount = parseEther('1999989.9');
 
         await expect(
             contract.transfer_substrate(destination.addressRaw, amount, {
