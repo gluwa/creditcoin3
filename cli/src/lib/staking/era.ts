d320a9b9dd954e91ce48c58d8beaba562c90e9da
Alex Todorov <alex.todorov@gluwa.com>
2024-04-30 15:06:12 +0300

    Remove unused function timeTillEra()
    b/c it contains a call to api.query.staking.currentEra() which seems to
    be missing or has been removed, see
    https://github.com/polkadot-js/api/issues/2040

--- cli/src/lib/staking/era.ts
+++ cli/src/lib/staking/era.ts
@@ -1,24 +1,4 @@
-import { ApiPromise, BN } from '..';
-
-// Returns time in milliseconds contained in a BN
-export async function timeTillEra(api: ApiPromise, era: number) {
-    const eraNumber = new BN(era);
-    const currentEra = new BN((await api.query.staking.currentEra()).toString());
-    const eraLength = new BN(api.consts.staking.sessionsPerEra.toString());
-    const slotsPerEpoch = new BN(api.consts.babe.epochDuration.toString());
-    const slotsPerEra = eraLength.mul(slotsPerEpoch);
-    const eraProgress = new BN((await api.derive.session.eraProgress()).toString());
-    const blocksTillNextEra = slotsPerEra.sub(eraProgress);
-    const minBlockTime = new BN(api.consts.babe.expectedBlockTime.toString());
-
-    const timeTillTargetEra = eraNumber
-        .sub(currentEra.add(new BN(1)))
-        .mul(slotsPerEra)
-        .add(blocksTillNextEra)
-        .mul(minBlockTime);
-
-    return timeTillTargetEra;
-}
+import { ApiPromise } from '..';
 
 export async function checkEraIsInHistory(era: number, api: ApiPromise): Promise<boolean> {
     const currentEra = (await api.query.staking.currentEra()).value.toNumber();
