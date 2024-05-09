5fd4a6b6c85605be80134d9cdae55229de79b083
Alex Todorov <alex.todorov@gluwa.com>
2024-05-09 13:00:19 +0300

    Increase timeouts before integration tests
    - wait max 20 seconds for Alice & Bob nodes to start
    - wait max 10 seconds to make sure the chain is finalizing before
      continuing with individual testing scenarios
    
    This should make test execution on self-hosted runners more stable b/c
    they don't have temporary local disks and startup times appear to be
    slower.

--- cli/src/test/integrationTestSetupAfterEnv.ts
+++ cli/src/test/integrationTestSetupAfterEnv.ts
@@ -5,7 +5,7 @@ global.beforeAll(async () => {
     // setup/teardown code in there. But make sure to have a separate function
     // which is only intended to be used at the outer-most layer
     await startAliceAndBob();
-}, 10_000);
+}, 20_000);
 
 global.afterAll(() => {
     killCreditcoinNodes();
@@ -14,4 +14,4 @@ global.afterAll(() => {
 global.beforeEach(async () => {
     // short-circuit test execution if the chain has stopped finalizing
     await expectIsFinalizing();
-});
+}, 10_000);
