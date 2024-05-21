89ef12cc7882e5124fd237e4c5d31a5b6ac1a8ad
Alex Todorov <alex.todorov@gluwa.com>
2024-05-21 14:48:53 +0300

    Add CI job to exercise the Hardhat examples
    plus a network configuration to deploy to creditcoin_local
    
    WARNING: Use a different configuration file when deploying to creditcoin_testnet
    otherwise local development will need to define a hardhat variable!

--- docs/smart-contract-development/with-hardhat/hardhat.config.ts
+++ docs/smart-contract-development/with-hardhat/hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    creditcoin_local: {
+      url: "http://127.0.0.1:9944",
+      // EVM private keys for development accounts are documented at
+      // https://docs.moonbeam.network/builders/get-started/networks/moonbeam-dev/#pre-funded-development-accounts
+      // This is the account Balthathar !
+      accounts: ["0x8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b"]
+    }
+  }
 };
 
 export default config;
