f9182987b4404c34270a6a42be2830dab258a796
Alex Todorov <alex.todorov@gluwa.com>
2024-05-30 16:45:44 +0300

    Add config for deploying to a running Hardhat node

--- docs/smart-contract-development/with-hardhat/hardhat.config.ts
+++ docs/smart-contract-development/with-hardhat/hardhat.config.ts
@@ -10,6 +10,12 @@ const config: HardhatUserConfig = {
       // https://docs.moonbeam.network/builders/get-started/networks/moonbeam-dev/#pre-funded-development-accounts
       // This is the account Balthathar !
       accounts: ["0x8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b"]
+    },
+    hardhat_local: {
+      url: "http://127.0.0.1:8545",
+      // EVM private keys are printed when starting npx hardhat node
+      // This is Account #0
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
     }
   }
 };
