af8a564c47f020a4524857fc1f85172b6b8d2b92
Alex Todorov <alex.todorov@gluwa.com>
2024-05-30 16:56:44 +0300

    Move MM prompt inside branch
    will execute only when MetaMask is installed

--- docs/smart-contract-development/with-ethers.js/script.js
+++ docs/smart-contract-development/with-ethers.js/script.js
@@ -8,34 +8,32 @@ let CounterContract;
 const CounterContractAddress = "DEPLOYED_COUNTER_CONTRACT_ADDRESS";
 const CounterContractABI = CounterContractArtifact.abi;
 
-window.ethereum.request({
-  method: "wallet_addEthereumChain",
-  params: [{
-      chainId: "0x18e8f", // see https://chainlist.org/chain/102031
-      rpcUrls: ["https://rpc.cc3-testnet.creditcoin.network"],
-      chainName: "Creditcoin Testnet",
-      nativeCurrency: {
-          name: "tCTC",
-          symbol: "tCTC",
-          decimals: 18
-      },
-      blockExplorerUrls: ["https://creditcoin-testnet.blockscout.com/"]
-  }]
-});
-
-if (window.ethereum == null) {
-
+if (window.ethereum === null) {
     // If MetaMask is not installed, we use the default provider,
     // which is backed by a variety of third-party services (such
     // as INFURA). They do not have private keys installed,
     // so they only have read-only access
     console.log("MetaMask not installed; using read-only defaults")
     provider = ethers.getDefaultProvider()
-
 } else {
     // Connect to the MetaMask EIP-1193 object. This is a standard
     // protocol that allows Ethers access to make all read-only
     // requests through MetaMask.
+
+    const result = await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [{
+          chainId: "0x18e8f", // see https://chainlist.org/chain/102031
+          rpcUrls: ["https://rpc.cc3-testnet.creditcoin.network"],
+          chainName: "Creditcoin Testnet",
+          nativeCurrency: {
+              name: "tCTC",
+              symbol: "tCTC",
+              decimals: 18
+          },
+          blockExplorerUrls: ["https://creditcoin-testnet.blockscout.com/"]
+      }]
+    });
 
     // WARNING: this works with ethers.js v6 or later
     provider = new ethers.BrowserProvider(window.ethereum)
