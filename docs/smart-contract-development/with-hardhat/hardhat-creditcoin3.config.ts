ffb262556cda6cc935cc9e674337ee9c673ed40c
Alex Todorov <alex.todorov@gluwa.com>
2024-06-05 13:03:27 +0300

    Add a missing import which doesn't seem to be needed
    b/c we've got CI steps which already successfully deploy to Devnet
    without this import. For example:
    https://github.com/gluwa/creditcoin3/actions/runs/9369193529/job/25796327016?pr=380

--- docs/smart-contract-development/with-hardhat/hardhat-creditcoin3.config.ts
+++ docs/smart-contract-development/with-hardhat/hardhat-creditcoin3.config.ts
@@ -1,4 +1,4 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 // Replace this with your Creditcoin3 Testnet account private key
