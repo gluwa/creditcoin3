7cbc8767cb23dd289404a8f83b73bda5cc0cc8c0
Alex Todorov <alex.todorov@gluwa.com>
2024-06-10 19:17:37 +0300

    Bump version to 3.29.0

--- runtime/src/version.rs
+++ runtime/src/version.rs
@@ -7,7 +7,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("creditcoin3"),
     impl_name: create_runtime_str!("creditcoin3"),
     authoring_version: 3,
-    spec_version: 28,
+    spec_version: 29,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
