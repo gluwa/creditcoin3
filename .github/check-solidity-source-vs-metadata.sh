22bd743338b6d2a74805249278b735537e45ffed
Alex Todorov <alex.todorov@gluwa.com>
2024-06-11 13:53:43 +0300

    Check precompile ABI vs metadata JSON

--- .github/check-solidity-source-vs-metadata.sh
+++ .github/check-solidity-source-vs-metadata.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SRC_FROM_DISK=$(cat precompiles/metadata/sol/*.sol)
 # NOTE: jq will interpret escape sequences so this value should equal to the raw code on disk
-SRC_FROM_JSON=$(jq -r .[0].source precompiles/metadata/precompiles-creditcoin3-devnet.json)
+SRC_FROM_JSON=$(jq -r .[].source precompiles/metadata/precompiles-creditcoin3-devnet.json)
 
 if [ "$SRC_FROM_DISK" == "$SRC_FROM_JSON" ]; then
     echo "INFO: Sources on disk match sources in JSON file"
@@ -21,7 +21,7 @@ else
 fi
 
 ADDRESS_FROM_DISK=$(grep "address constant" precompiles/metadata/sol/*.sol | cut -f2 -d'=' | tr -d ' ;')
-ADDRESS_FROM_JSON=$(jq -r .[0].address precompiles/metadata/precompiles-creditcoin3-devnet.json)
+ADDRESS_FROM_JSON=$(jq -r .[].address precompiles/metadata/precompiles-creditcoin3-devnet.json)
 
 if [ "$ADDRESS_FROM_DISK" == "$ADDRESS_FROM_JSON" ]; then
     echo "INFO: Address on disk matches address in JSON file"
@@ -33,4 +33,23 @@ else
     echo "========================"
 
     exit 2
+fi
+
+
+# NOTE: requires that abi-creator.sh was executed beforehand
+# NOTE2: both representations are multi-line
+ABI_FROM_DISK=$(jq -r "..|.abi?|select(.)" precompiles/metadata/abi/*.json)
+ABI_FROM_JSON=$(jq -r .[].abi precompiles/metadata/precompiles-creditcoin3-devnet.json | jq -r)
+
+# TODO: compare against ABI in integration tests as well
+if [ "$ABI_FROM_DISK" == "$ABI_FROM_JSON" ]; then
+    echo "INFO: ABI on disk matches ABI in JSON file"
+else
+    echo "FAIL: ABI on disk differs from ABI in JSON file"
+
+    echo "FROM_DISK=$ABI_FROM_DISK"
+    echo "FROM_JSON=$ABI_FROM_JSON"
+    echo "========================"
+
+    exit 3
 fi
