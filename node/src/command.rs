de6f3fccbef3499004ee9dc5fe3cd37406ab6981
Beqa Abuladze <beqaabuladze.00@gmail.com>
2024-08-29 17:06:54 +0400

    chore: fmt

--- node/src/command.rs
+++ node/src/command.rs
@@ -67,7 +67,7 @@ impl SubstrateCli for Cli {
             "testnet" => Box::new(chain_spec::testnet_config()?),
             "" | "local" => Box::new(chain_spec::local_testnet_config()),
             "dryrun" => Box::new(chain_spec::dry_run_config()?),
-            "mainnet"=> Box::new(chain_spec::mainnet_config()?),
+            "mainnet" => Box::new(chain_spec::mainnet_config()?),
             path => Box::new(chain_spec::ChainSpec::from_json_file(
                 std::path::PathBuf::from(path),
             )?),
