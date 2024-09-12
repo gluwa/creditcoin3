ec0b891ac3375984338be9dce95c40b07c53f579
dylanverstraete <dylan_verstraete1@hotmail.com>
2024-09-12 19:30:35 +0200

    fix: correct precompile interface and json abi

--- precompiles/metadata/sol/substrate_transfer.sol
+++ precompiles/metadata/sol/substrate_transfer.sol
@@ -4,14 +4,16 @@ pragma solidity >=0.8.3;
 /// @dev The Deposit precompile address
 address constant SUBSTRATE_TRANSFER_ADDRESS = 0x0000000000000000000000000000000000000Fd1;
 
-SubstrateTransfer constant SUBSTRATE_TRANSFER_ADRRESS = SubstrateTransfer(SUBSTRATE_TRANSFER_ADDRESS);
+SubstrateTransfer constant SUBSTRATE_TRANSFER_ADRRESS = SubstrateTransfer(
+    SUBSTRATE_TRANSFER_ADDRESS
+);
 
 /// @title SubstrateTransfer interface
 interface SubstrateTransfer {
     /// @dev Event emitted when a transfer has been performed.
     /// @param destination The Substrate address receiving the tokens.
     /// @param amount The amount of tokens transferred.
-    event Transfer(bytes32 destination, uint256 amount);
+    event Transfer(bytes32 indexed destination, uint256 indexed amount);
 
     /// @dev Function to transfer tokens to a Substrate address.
     /// @param destination The Substrate address receiving the tokens.
