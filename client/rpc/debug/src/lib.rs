293db3985b46b87f0c42061ddd7c6c1753e8b9dc
rustlang-dev <rust.lang.developer@gmail.com>
2024-09-12 13:37:35 +0100

    fix overrides evm cache task.

--- client/rpc/debug/src/lib.rs
+++ client/rpc/debug/src/lib.rs
@@ -16,6 +16,7 @@
 pub use creditcoin3_rpc_core_debug::{DebugServer, TraceParams};
 use futures::StreamExt;
 use jsonrpsee::core::{async_trait, RpcResult};
+use fc_storage::StorageOverride;
 
 use tokio::{
     self,
@@ -26,7 +27,8 @@ use creditcoin3_client_evm_tracing::{formatters::ResponseFormatter, types::singl
 use creditcoin3_rpc_core_types::{RequestBlockId, RequestBlockTag};
 use creditcoin3_rpc_primitives_debug::{DebugRuntimeApi, TracerInput};
 use ethereum_types::H256;
-use fc_rpc::{frontier_backend_client, internal_err, OverrideHandle};
+// use fc_rpc::{frontier_backend_client, internal_err, OverrideHandle};
+use fc_rpc::{frontier_backend_client, internal_err};
 use fp_rpc::EthereumRuntimeRPCApi;
 use sc_client_api::backend::{Backend, StateBackend, StorageProvider};
 use sc_utils::mpsc::TracingUnboundedSender;
@@ -149,7 +151,7 @@ where
         backend: Arc<BE>,
         frontier_backend: Arc<dyn fc_api::Backend<B> + Send + Sync>,
         permit_pool: Arc<Semaphore>,
-        overrides: Arc<OverrideHandle<B>>,
+        overrides: Arc<dyn StorageOverride<B>>,
         raw_max_memory_usage: usize,
     ) -> (impl Future<Output = ()>, DebugRequester) {
         let (tx, mut rx): (DebugRequester, _) =
@@ -290,7 +292,7 @@ where
         frontier_backend: Arc<dyn fc_api::Backend<B> + Send + Sync>,
         request_block_id: RequestBlockId,
         params: Option<TraceParams>,
-        overrides: Arc<OverrideHandle<B>>,
+        overrides: Arc<dyn StorageOverride<B>>,
     ) -> RpcResult<Response> {
         let (tracer_input, trace_type) = Self::handle_params(params)?;
 
@@ -335,21 +337,13 @@ where
         let parent_block_hash = *header.parent_hash();
 
         // let schema = fc_storage::onchain_storage_schema::<B, C, BE>(client.as_ref(), hash);
-        let schema = client.storage_schema::<B, C, BE>(hash);
+        // let schema = client.storage_schema::<B, C, BE>(hash);
 
         // Using storage overrides we align with `:ethereum_schema` which will result in proper
         // SCALE decoding in case of migration.
-        let statuses = match overrides.schemas.get(&schema) {
-            Some(schema) => schema
-                .current_transaction_statuses(hash)
-                .unwrap_or_default(),
-            _ => {
-                return Err(internal_err(format!(
-                    "No storage override at {:?}",
-                    reference_id
-                )))
-            }
-        };
+        let statuses = overrides
+            .current_transaction_statuses(hash)
+            .unwrap_or_default();
 
         // Known ethereum transaction hashes.
         let eth_tx_hashes: Vec<_> = statuses.iter().map(|t| t.transaction_hash).collect();
@@ -425,7 +419,7 @@ where
         frontier_backend: Arc<dyn fc_api::Backend<B> + Send + Sync>,
         transaction_hash: H256,
         params: Option<TraceParams>,
-        overrides: Arc<OverrideHandle<B>>,
+        overrides: Arc<dyn StorageOverride<B>>,
         raw_max_memory_usage: usize,
     ) -> RpcResult<Response> {
         let (tracer_input, trace_type) = Self::handle_params(params)?;
@@ -484,27 +478,19 @@ where
             ));
         };
 
-        let x = client.storage(reference_hash, &sc_client_api::StorageKey(b":ethereum_schema".to_vec()));
-        let schema = match x {
-            Ok(Some(bytes)) => parity_scale_codec::Decode::decode(&mut &bytes.0[..])
-                .ok()
-                .unwrap_or(fp_storage::EthereumStorageSchema::Undefined),
-            _ => fp_storage::EthereumStorageSchema::Undefined,
-        };
+        // let x = client.storage(reference_hash, &sc_client_api::StorageKey(b":ethereum_schema".to_vec()));
+        // let schema = match x {
+        //     Ok(Some(bytes)) => parity_scale_codec::Decode::decode(&mut &bytes.0[..])
+        //         .ok()
+        //         .unwrap_or(fp_storage::EthereumStorageSchema::Undefined),
+        //     _ => fp_storage::EthereumStorageSchema::Undefined,
+        // };
         // let schema = fc_storage::onchain_storage_schema::<B, C, BE>(client.as_ref(), reference_hash);
 
 
         // Get the block that contains the requested transaction. Using storage overrides we align
         // with `:ethereum_schema` which will result in proper SCALE decoding in case of migration.
-        let reference_block = match overrides.schemas.get(&schema) {
-            Some(schema) => schema.current_block(reference_hash),
-            _ => {
-                return Err(internal_err(format!(
-                    "No storage override at {:?}",
-                    reference_hash
-                )))
-            }
-        };
+        let reference_block = overrides.current_block(reference_hash);
 
         // Get the actual ethereum transaction.
         if let Some(block) = reference_block {
