d7d8f01780f50c48a44d077610229398cde67bb8
BradleyOlson64 <lotrftw9@gmail.com>
2024-09-20 10:28:54 -0700

    Fixing db down migration

--- prover/migrations/2024-06-24-150504_attestations/down.sql
+++ prover/migrations/2024-06-24-150504_attestations/down.sql
@@ -2,12 +2,8 @@
 DROP INDEX IF EXISTS block_with_digest_idx_chain_id;
 DROP INDEX IF EXISTS block_with_digest_idx_header_number;
 DROP INDEX IF EXISTS block_with_digest_idx_digest;
--- Drop indexes on chain_id, header_number, and digest
-DROP INDEX IF EXISTS signed_attestation_idx_chain_id;
-DROP INDEX IF EXISTS signed_attestation_idx_header_number;
-DROP INDEX IF EXISTS signed_attestation_idx_digest;
 
--- Drop indexes on chain_id, header_number, and digest
+-- Drop Attestation indices
 DROP INDEX IF EXISTS attestation_idx_chain_id;
 DROP INDEX IF EXISTS attestation_idx_header_number;
 DROP INDEX IF EXISTS attestation_idx_digest;
@@ -20,8 +16,8 @@ DROP INDEX IF EXISTS attestation_checkpoint_idx_digest;
 -- Drop the table for BlockWithDigest
 DROP TABLE IF EXISTS BlockWithDigest;
 
--- Drop the table for AttestationCheckpoint
-DROP TABLE IF EXISTS AttestationCheckpoint;
-
 -- Drop the table for Attestation
 DROP TABLE IF EXISTS Attestation;
+
+-- Drop the table for AttestationCheckpoint
+DROP TABLE IF EXISTS AttestationCheckpoint;
