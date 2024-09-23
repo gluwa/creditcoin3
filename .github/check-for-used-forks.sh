38018044b3fd1d622d55013a561128e74f072886
rustlang-dev <rust.lang.developer@gmail.com>
2024-09-23 14:47:56 +0100

    whitelist github.com/paritytech frontier and polkadot-sdk

--- .github/check-for-used-forks.sh
+++ .github/check-for-used-forks.sh
@@ -11,8 +11,8 @@ USED_FORKS=$(find ./ -name Cargo.toml -print0 | xargs --null grep git |
 # whitelist begin
     grep -v "repository =" |
     # frontier doesn't publish to crates.io and depends on polkadot-sdk
-    # grep -v github.com/gluwa/frontier |
-    # grep -v github.com/gluwa/polkadot-sdk |
+    grep -v github.com/paritytech/frontier |
+    grep -v github.com/paritytech/polkadot-sdk |
     # frontier depends on this specific version
     grep -v github.com/rust-blockchain/evm
 # whitelist end
