9a91aef98d3cf3c5967806735395129211745adf
Alex Todorov <alex.todorov@gluwa.com>
2024-10-25 14:27:58 +0300

    Update sanity test to assert on multiple source chains
    also assert on AttestorElected events

--- cli/src/test/attestation-tests/sanity.test.ts
+++ cli/src/test/attestation-tests/sanity.test.ts
@@ -23,9 +23,23 @@ describe('BlockAttested events', (): void => {
     });
 
     test('are emitted frequently enough and match Ethereum', async (): Promise<void> => {
-        let previousDigest = '';
-        let previousHeader = 0;
-        let attestedEvents = 0;
+        /* eslint-disable @typescript-eslint/naming-convention */
+        const previousDigest: { [key: string]: string } = {
+            '2': '',
+            '4': '',
+        };
+        const previousHeader: { [key: string]: number } = {
+            '2': 0,
+            '4': 0,
+        };
+        const attestedEvents: { [key: string]: number } = {
+            '2': 0,
+            '4': 0,
+        };
+        const electionEvents: { [key: string]: number } = {
+            '2': 0,
+            '4': 0,
+        };
         const initialBlock = (await getChainStatus(api)).bestNumber;
 
         return new Promise((resolve, reject): void => {
@@ -42,6 +56,7 @@ describe('BlockAttested events', (): void => {
                             // Show what we are busy with
                             console.log(`EVENT=${event.section}:${event.method}; data=${event.data.toString()}`);
                             const [chainId, signedAttn, digest] = event.data;
+                            const chainIdStr = (chainId as U64).toString();
                             const data = signedAttn as AttestorPrimitivesSignedAttestation;
 
                             const chainAttestationInterval = (
@@ -49,21 +64,30 @@ describe('BlockAttested events', (): void => {
                             ).toNumber();
 
                             // external blocks should be attested at the same interval which is recorded on-chain
-                            if (previousHeader > 0) {
-                                expect(data.attestation.headerNumber.toNumber() - previousHeader).toBe(
+                            if (previousHeader[chainIdStr] > 0) {
+                                expect(data.attestation.headerNumber.toNumber() - previousHeader[chainIdStr]).toBe(
                                     chainAttestationInterval,
                                 );
                             }
-                            previousHeader = data.attestation.headerNumber.toNumber();
+                            previousHeader[chainIdStr] = data.attestation.headerNumber.toNumber();
 
                             // recorded attestations should be linked to each other
-                            if (previousDigest !== '') {
-                                expect(data.attestation.prevDigest.toString()).toBe(previousDigest);
+                            if (previousDigest[chainIdStr] !== '') {
+                                expect(data.attestation.prevDigest.toString()).toBe(previousDigest[chainIdStr]);
                             }
                             // note: next attestation will point to the digest of the current one
-                            previousDigest = digest.toString();
+                            previousDigest[chainIdStr] = digest.toString();
 
-                            attestedEvents++;
+                            attestedEvents[chainIdStr]++;
+                        }
+
+                        if (`${event.section}.${event.method}` === 'attestation.AttestorsElected') {
+                            // Show what we are busy with
+                            console.log(`EVENT=${event.section}:${event.method}; data=${event.data.toString()}`);
+                            const [_epoch, chainId, _attestors] = event.data;
+                            const chainIdStr = (chainId as U64).toString();
+
+                            electionEvents[chainIdStr]++;
                         }
                     } // loop over events
 
@@ -74,15 +98,21 @@ describe('BlockAttested events', (): void => {
                 })
                 .catch((error) => reject(error));
         }).then(() => {
-            expect(attestedEvents).toBeGreaterThan(0);
+            expect(electionEvents['2']).toBeGreaterThanOrEqual(10);
+            expect(electionEvents['4']).toBeGreaterThanOrEqual(10);
+
+            expect(attestedEvents['2']).toBeGreaterThan(0);
+            expect(attestedEvents['4']).toBeGreaterThan(0);
 
             // 200 CC blocks is 1000 seconds which means around 166 Anvil blocks
             // ingested at 10 blocks this means 15-16 events max
-            expect(attestedEvents).toBeLessThanOrEqual(20);
+            expect(attestedEvents['2']).toBeLessThanOrEqual(20);
+            expect(attestedEvents['4']).toBeLessThanOrEqual(20);
 
             // match the frequency b/c we don't want this to pass if only a few events are recorded
             // and then something suddenly fails/disconnects
-            expect(attestedEvents).toBeGreaterThanOrEqual(10);
+            expect(attestedEvents['2']).toBeGreaterThanOrEqual(10);
+            expect(attestedEvents['4']).toBeGreaterThanOrEqual(10);
             // note that this isn't super robust b/c we still don't quite know what the
             // average distance between these events is, see CSUB-1268 but
             // nevertheless should be good enough for CI to detect if something suddenly
