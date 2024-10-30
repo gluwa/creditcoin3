fe9781b046d56221c8c9972d935e667de6a7fdbd
Alex Todorov <alex.todorov@gluwa.com>
2024-10-30 11:57:46 +0200

    Add integration test for attestation.setAttestationsPerCheckpoint()

---
+++ cli/src/test/blockchain-tests/pallets/attestation/set-attestations-per-checkpoint.test.ts
@@ -0,0 +1,31 @@
+import { newApi, ApiPromise, KeyringPair } from '../../../../lib';
+import { extractFee } from '../../../utils';
+import { chain_Anvil2_Key } from '../supported-chains/consts';
+
+describe('SetAttestationsPerCheckpoint', (): void => {
+    let api: ApiPromise;
+    let root: KeyringPair;
+
+    beforeAll(async () => {
+        ({ api } = await newApi((global as any).CREDITCOIN_API_URL));
+        root = (global as any).CREDITCOIN_CREATE_SIGNER('sudo');
+    });
+
+    afterAll(async () => {
+        await api.disconnect();
+    });
+
+    it('fee is min 0.01 CTC', async (): Promise<void> => {
+        return new Promise((resolve, reject): void => {
+            // note: use chain Anvil2 b/c this may lead to side effects in other test scenarios
+            const unsubscribe = api.tx.sudo
+                .sudo(api.tx.attestation.setAttestationsPerCheckpoint(chain_Anvil2_Key, 4))
+                .signAndSend(root, { nonce: -1 }, async ({ dispatchError, events, status }) => {
+                    await extractFee(resolve, reject, unsubscribe, api, dispatchError, events, status);
+                })
+                .catch((error) => reject(error));
+        }).then((fee) => {
+            expect(fee).toBeGreaterThanOrEqual((global as any).CREDITCOIN_MINIMUM_TXN_FEE);
+        });
+    }, 30_000);
+});
