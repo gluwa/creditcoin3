c19d75034b90a1603b557e6cf03fa285eac004ec
Alex Todorov <alex.todorov@gluwa.com>
2024-12-13 11:55:29 +0200

    Add integration test for prover.removeStarkProgramMetadata

---
+++ cli/src/test/blockchain-tests/pallets/prover/remove-stark-program-metadata.test.ts
@@ -0,0 +1,36 @@
+import { newApi, ApiPromise, KeyringPair } from '../../../../lib';
+import { extractFee } from '../../../utils';
+import { starkProgramHash, starkProgramVersion } from './consts';
+
+describe('RemoveStarkProgramMetadata', (): void => {
+    let api: ApiPromise;
+    let root: KeyringPair;
+
+    beforeAll(async () => {
+        ({ api } = await newApi((global as any).CREDITCOIN_API_URL));
+        root = (global as any).CREDITCOIN_CREATE_SIGNER('sudo');
+
+        // set metadata if it was previously removed.
+        // will fail silently if already set
+        await api.tx.sudo
+            .sudo(api.tx.prover.setStarkProgramMetadata(starkProgramVersion, starkProgramHash))
+            .signAndSend(root);
+    }, 30_000);
+
+    afterAll(async () => {
+        await api.disconnect();
+    });
+
+    it('fee is min 0.01 CTC', async (): Promise<void> => {
+        return new Promise((resolve, reject): void => {
+            const unsubscribe = api.tx.sudo
+                .sudo(api.tx.prover.removeStarkProgramMetadata(starkProgramVersion))
+                .signAndSend(root, { nonce: -1 }, async ({ dispatchError, events, status }) => {
+                    await extractFee(resolve, reject, unsubscribe, api, dispatchError, events, status);
+                })
+                .catch((error) => reject(error));
+        }).then((fee) => {
+            expect(fee).toBeGreaterThanOrEqual((global as any).CREDITCOIN_MINIMUM_TXN_FEE);
+        });
+    }, 30_000);
+});
