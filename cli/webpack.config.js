64c4564ddda103dae6545379ee6a3e00ce657e96
rustlang-dev <rust.lang.developer@gmail.com>
2024-12-18 15:35:08 +0000

    configs

---
+++ cli/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/cli.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: /node_modules|(tests|\.test\.ts$)/,
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+      "process": require.resolve("process/browser"),
+      "buffer": require.resolve("buffer/"),
+    },
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+};
