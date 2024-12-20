473a2ab23ff7437807b420d4fa04e92fa3d167fe
rustlang-dev <rust.lang.developer@gmail.com>
2024-12-20 15:44:58 +0000

    added unregisterAttestorAction into attestor cli

---
+++ cli/src/commands/attestor/unregisterAttestor.ts
@@ -0,0 +1,30 @@
+import { Command, OptionValues } from 'commander';
+import { getValidatorStatus, newApi, requireStatus } from '../../lib';
+import { requireKeyringHasSufficientFunds, signSendAndWatchCcKeyring } from '../../lib/tx';
+import { initKeyring, delegateAddress } from '../../lib/account/keyring';
+import { proxyForOption } from '../options';
+
+export function makeUnregisterAttestorCommand() {
+    const cmd = new Command('unregister-attestor');
+    cmd.description('Unregister attestor and unbond funds from a stash account');
+    cmd.addOption(proxyForOption);
+    cmd.action(unregisterAttestorAction);
+    return cmd;
+}
+
+async function unregisterAttestorAction(options: OptionValues) {
+    const { api } = await newApi(options.url as string);
+
+    const chainKey = options.chainKey as string;
+
+    const keyring = await initKeyring(options);
+
+    const validatorAddr = delegateAddress(keyring);
+
+    const unregisterAttestorTx = api.tx.attestation.unregisterAttestor(chainKey, validatorAddr);
+
+    await requireKeyringHasSufficientFunds(unregisterAttestorTx, keyring, api);
+    const result = await signSendAndWatchCcKeyring(unregisterAttestorTx, api, keyring);
+    console.log(result.info);
+    process.exit(result.status);
+}
