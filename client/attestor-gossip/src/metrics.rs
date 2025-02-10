5600a4be9230fc37cbfe94b829651ff90047fdf5
rustlang-dev <rust.lang.developer@gmail.com>
2025-02-10 16:12:55 +0000

    cargo fmt --all

--- client/attestor-gossip/src/metrics.rs
+++ client/attestor-gossip/src/metrics.rs
@@ -4,153 +4,171 @@ use substrate_prometheus_endpoint::{register, Counter, Gauge, PrometheusError, R
 
 /// Helper trait for registering attestor metrics to Prometheus registry.
 pub(crate) trait PrometheusRegister<T: Sized = Self>: Sized {
-	const DESCRIPTION: &'static str;
-	fn register(registry: &Registry) -> Result<Self, PrometheusError>;
+    const DESCRIPTION: &'static str;
+    fn register(registry: &Registry) -> Result<Self, PrometheusError>;
 }
 
 /// attestor voting-related metrics exposed through Prometheus
 #[derive(Clone, Debug)]
 pub struct VoterMetrics {
-	pub attestor_votes_sent: Counter<U64>,
-	/// Best block finalized by attestor
-	pub attestor_best_block: Gauge<U64>,
-	/// Best block attestor voted on
-	pub attestor_best_voted: Gauge<U64>,
-	/// Next block attestor should vote on
-	pub attestor_should_vote_on: Gauge<U64>,
-	/// Number of times no Authority public key found in store
-	pub attestor_no_authority_found_in_store: Counter<U64>,
-	/// Number of good votes successfully handled
-	pub attestor_good_votes_processed: Counter<U64>,
-	/// Number of equivocation votes received
-	pub attestor_equivocation_votes: Counter<U64>,
-	/// Number of invalid votes received
-	pub attestor_invalid_votes: Counter<U64>,
-	/// Number of valid but stale votes received
-	pub attestor_stale_votes: Counter<U64>,
+    pub attestor_votes_sent: Counter<U64>,
+    /// Best block finalized by attestor
+    pub attestor_best_block: Gauge<U64>,
+    /// Best block attestor voted on
+    pub attestor_best_voted: Gauge<U64>,
+    /// Next block attestor should vote on
+    pub attestor_should_vote_on: Gauge<U64>,
+    /// Number of times no Authority public key found in store
+    pub attestor_no_authority_found_in_store: Counter<U64>,
+    /// Number of good votes successfully handled
+    pub attestor_good_votes_processed: Counter<U64>,
+    /// Number of equivocation votes received
+    pub attestor_equivocation_votes: Counter<U64>,
+    /// Number of invalid votes received
+    pub attestor_invalid_votes: Counter<U64>,
+    /// Number of valid but stale votes received
+    pub attestor_stale_votes: Counter<U64>,
     /// Number of valid votes successfully imported
     pub attestor_imported_votes: Counter<U64>,
 }
 
 impl PrometheusRegister for VoterMetrics {
-	const DESCRIPTION: &'static str = "voter";
-	fn register(registry: &Registry) -> Result<Self, PrometheusError> {
-		Ok(Self {
-			attestor_votes_sent: register(
-				Counter::new("substrate_attestor_votes_sent", "Number of votes sent by this node")?,
-				registry,
-			)?,
-			attestor_best_block: register(
-				Gauge::new("substrate_attestor_best_block", "Best block finalized by attestor")?,
-				registry,
-			)?,
-			attestor_best_voted: register(
-				Gauge::new("substrate_attestor_best_voted", "Best block voted on by attestor")?,
-				registry,
-			)?,
-			attestor_should_vote_on: register(
-				Gauge::new("substrate_attestor_should_vote_on", "Next block, attestor should vote on")?,
-				registry,
-			)?,
-			attestor_no_authority_found_in_store: register(
-				Counter::new(
-					"substrate_attestor_no_authority_found_in_store",
-					"Number of times no Authority public key found in store",
-				)?,
-				registry,
-			)?,
-			attestor_good_votes_processed: register(
-				Counter::new(
-					"substrate_attestor_successful_handled_votes",
-					"Number of good votes successfully handled",
-				)?,
-				registry,
-			)?,
-			attestor_equivocation_votes: register(
-				Counter::new(
-					"substrate_attestor_equivocation_votes",
-					"Number of equivocation votes received",
-				)?,
-				registry,
-			)?,
-			attestor_invalid_votes: register(
-				Counter::new("substrate_attestor_invalid_votes", "Number of invalid votes received")?,
-				registry,
-			)?,
-			attestor_stale_votes: register(
-				Counter::new(
-					"substrate_attestor_stale_votes",
-					"Number of valid but stale votes received",
-				)?,
-				registry,
-			)?,
+    const DESCRIPTION: &'static str = "voter";
+    fn register(registry: &Registry) -> Result<Self, PrometheusError> {
+        Ok(Self {
+            attestor_votes_sent: register(
+                Counter::new(
+                    "substrate_attestor_votes_sent",
+                    "Number of votes sent by this node",
+                )?,
+                registry,
+            )?,
+            attestor_best_block: register(
+                Gauge::new(
+                    "substrate_attestor_best_block",
+                    "Best block finalized by attestor",
+                )?,
+                registry,
+            )?,
+            attestor_best_voted: register(
+                Gauge::new(
+                    "substrate_attestor_best_voted",
+                    "Best block voted on by attestor",
+                )?,
+                registry,
+            )?,
+            attestor_should_vote_on: register(
+                Gauge::new(
+                    "substrate_attestor_should_vote_on",
+                    "Next block, attestor should vote on",
+                )?,
+                registry,
+            )?,
+            attestor_no_authority_found_in_store: register(
+                Counter::new(
+                    "substrate_attestor_no_authority_found_in_store",
+                    "Number of times no Authority public key found in store",
+                )?,
+                registry,
+            )?,
+            attestor_good_votes_processed: register(
+                Counter::new(
+                    "substrate_attestor_successful_handled_votes",
+                    "Number of good votes successfully handled",
+                )?,
+                registry,
+            )?,
+            attestor_equivocation_votes: register(
+                Counter::new(
+                    "substrate_attestor_equivocation_votes",
+                    "Number of equivocation votes received",
+                )?,
+                registry,
+            )?,
+            attestor_invalid_votes: register(
+                Counter::new(
+                    "substrate_attestor_invalid_votes",
+                    "Number of invalid votes received",
+                )?,
+                registry,
+            )?,
+            attestor_stale_votes: register(
+                Counter::new(
+                    "substrate_attestor_stale_votes",
+                    "Number of valid but stale votes received",
+                )?,
+                registry,
+            )?,
             attestor_imported_votes: register(
-				Counter::new(
-					"attestor_imported_votes",
-					"Number of valid votes successfully imported",
-				)?,
-				registry,
-			)?,
-		})
-	}
+                Counter::new(
+                    "attestor_imported_votes",
+                    "Number of valid votes successfully imported",
+                )?,
+                registry,
+            )?,
+        })
+    }
 }
 
 pub(crate) fn register_metrics<T: PrometheusRegister>(
-	prometheus_registry: Option<substrate_prometheus_endpoint::Registry>,
+    prometheus_registry: Option<substrate_prometheus_endpoint::Registry>,
 ) -> Option<T> {
-	prometheus_registry.as_ref().map(T::register).and_then(|result| match result {
-		Ok(metrics) => {
-			debug!(target: LOG_TARGET, "🥩 Registered {} metrics", T::DESCRIPTION);
-			Some(metrics)
-		},
-		Err(err) => {
-			error!(
-				target: LOG_TARGET,
-				"🥩 Failed to register {} metrics: {:?}",
-				T::DESCRIPTION,
-				err
-			);
-			None
-		},
-	})
+    prometheus_registry
+        .as_ref()
+        .map(T::register)
+        .and_then(|result| match result {
+            Ok(metrics) => {
+                debug!(target: LOG_TARGET, "🥩 Registered {} metrics", T::DESCRIPTION);
+                Some(metrics)
+            }
+            Err(err) => {
+                error!(
+                    target: LOG_TARGET,
+                    "🥩 Failed to register {} metrics: {:?}",
+                    T::DESCRIPTION,
+                    err
+                );
+                None
+            }
+        })
 }
 
 // Note: we use the `format` macro to convert an expr into a `u64`. This will fail,
 // if expr does not derive `Display`.
 #[macro_export]
 macro_rules! metric_set {
-	($metrics:expr, $m:ident, $v:expr) => {{
-		let val: u64 = format!("{}", $v).parse().unwrap();
+    ($metrics:expr, $m:ident, $v:expr) => {{
+        let val: u64 = format!("{}", $v).parse().unwrap();
 
-		if let Some(metrics) = $metrics.as_ref() {
-			metrics.$m.set(val);
-		}
-	}};
+        if let Some(metrics) = $metrics.as_ref() {
+            metrics.$m.set(val);
+        }
+    }};
 }
 
 #[macro_export]
 macro_rules! metric_inc {
-	($metrics:expr, $m:ident) => {{
-		if let Some(metrics) = $metrics.as_ref() {
-			metrics.$m.inc();
-		}
-	}};
+    ($metrics:expr, $m:ident) => {{
+        if let Some(metrics) = $metrics.as_ref() {
+            metrics.$m.inc();
+        }
+    }};
 }
 
 #[macro_export]
 macro_rules! metric_get {
-	($metrics:expr, $m:ident) => {{
-		$metrics.as_ref().map(|metrics| metrics.$m.clone())
-	}};
+    ($metrics:expr, $m:ident) => {{
+        $metrics.as_ref().map(|metrics| metrics.$m.clone())
+    }};
 }
 
 #[cfg(test)]
 pub(crate) mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn should_register_metrics() {
-		let registry = Some(Registry::new());
-		assert!(register_metrics::<VoterMetrics>(registry.clone()).is_some());
-	}
+    #[test]
+    fn should_register_metrics() {
+        let registry = Some(Registry::new());
+        assert!(register_metrics::<VoterMetrics>(registry.clone()).is_some());
+    }
 }
--- client/attestor-gossip/src/worker.rs
+++ client/attestor-gossip/src/worker.rs
@@ -1,3 +1,5 @@
+use crate::metrics::register_metrics;
+use crate::{metric_inc, metric_set, metrics::VoterMetrics};
 use attestor_primitives::ChainKey;
 use futures::{stream::Fuse, StreamExt};
 use log::{debug, error, info, warn};
@@ -8,11 +10,9 @@ use sp_consensus::SyncOracle;
 use sp_consensus_babe::BabeApi;
 use sp_core::H256;
 use sp_runtime::traits::{Block as BlockT, Hash as HashT, Header as HeaderT};
-use substrate_prometheus_endpoint::Registry;
 use std::fmt::{Debug, Display};
 use std::sync::Arc;
-use crate::metrics::register_metrics;
-use crate::{metric_inc, metric_set, metrics::VoterMetrics};
+use substrate_prometheus_endpoint::Registry;
 
 use attestor_primitives::{
     api::AttestorApi,
@@ -300,7 +300,6 @@ where
             return Err(Error::WorkerInSync);
         }
 
-
         let block_hash = self.backend.blockchain().info().best_hash;
 
         // Get the round for the attestation
@@ -340,7 +339,7 @@ where
             }
             VoteImportResult::Ok => {
                 let block_number = self.backend.blockchain().info().best_number;
-                let block_number : u64 = block_number.into();
+                let block_number: u64 = block_number.into();
                 metric_set!(self.metrics, attestor_best_voted, block_number);
                 info!(target: LOG_TARGET, "📝 Attestation added to round");
             }
