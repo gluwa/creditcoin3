08a6513cb6570660a558c86bec7d57c49de6a418
Alex Todorov <alex.todorov@gluwa.com>
2025-02-19 13:49:29 +0200

    Actually check that the ETH compatibility layer has started
    b/c it looks like the error
    
    missing field `mixHash` at line 1 column 1372
    
    happens when we try to deploy the prover smart contract *before* there
    are any eth blocks (or info about them) created on chain.
    
    Since a Substrate node starts multiple threads in parallel we just query
    the RPC for eth.blockNumber() and wait until that becomes non-zero.

---
+++ cli/src/scripts/eth-check.ts
@@ -0,0 +1,30 @@
+import { creditcoinApi } from '../lib/api';
+
+async function checkEthBlockNumber(wsUrl: string): Promise<void> {
+    const { api } = await creditcoinApi(wsUrl);
+
+    try {
+        const ethBlockNumber = await api.rpc.eth.blockNumber();
+
+        console.log(`DEBUG: eth.blockNumber=${ethBlockNumber.toString()}`);
+
+        // means the ETH compatibility layer isn't ready yet
+        if (ethBlockNumber.isZero()) {
+            throw new Error('eth.blockNumber() is zero');
+        }
+    } finally {
+        await api.disconnect();
+    }
+}
+
+if (process.argv.length < 3) {
+    console.error('USAGE: eth-check.ts <wsUrl>');
+    process.exit(1);
+}
+
+const inputWsUrl = process.argv[2];
+
+checkEthBlockNumber(inputWsUrl).catch((reason) => {
+    console.error(`ERROR: ${reason}`);
+    process.exit(1);
+});
