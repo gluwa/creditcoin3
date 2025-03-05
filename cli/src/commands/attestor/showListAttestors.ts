08d89bb948c39242f663596eea0fbc6f0b25743d
rustlang-dev <rust.lang.developer@gmail.com>
2025-03-05 16:50:05 +0000

    update showListAttestorsCommand.

--- cli/src/commands/attestor/showListAttestors.ts
+++ cli/src/commands/attestor/showListAttestors.ts
@@ -1,11 +1,8 @@
 import { Command, OptionValues } from 'commander';
-import { getValidatorStatus, newApi, requireStatus } from '../../lib';
-import { requireKeyringHasSufficientFunds, signSendAndWatchCcKeyring } from '../../lib/tx';
-import { initKeyring, delegateAddress } from '../../lib/account/keyring';
-import { proxyForOption } from '../options';
+import { newApi } from '../../lib';
 import { substrateAddressOption } from '../options';
 
-export function makeShowAttestorStatusCommand() {
+export function makeshowListAttestorsCommand() {
     const cmd = new Command('show-list-attestors');
     cmd.description('Show attestor status for a given address and chain key');
     cmd.addOption(substrateAddressOption.makeOptionMandatory());
@@ -13,35 +10,31 @@ export function makeShowAttestorStatusCommand() {
         '-c, --chain [chain]',
         'Specify chain key to show attestor status for',
     );
-    cmd.action(showUnclaimedRewardsAction);
+    cmd.action(showshowListAttestorsAction);
     return cmd;
 }
 
-async function showUnclaimedRewardsAction(options: OptionValues) {
+async function showshowListAttestorsAction(options: OptionValues) {
     const { api } = await newApi(options.url as string);
 
     const address = options.substrateAddress as string;
     const chainKey = options.chain as string;
 
-    const activeAttestors = await api.query.attestation.activeAttestors(chainKey);
-    for (let i = 0; i < activeAttestors.length; i++) {
-        if (activeAttestors[i].toString() === address) {
-            console.log(`Address ${address} status is Elected`);
-            process.exit(0);
+    const attestorsKeys = await api.query.attestation.attestors.keys();
+    for (let i = 0; i < attestorsKeys.length; i++) {
+        const key = attestorsKeys[i];
+        const chain = key.args[0].toString();
+        if (chain != chainKey) {
+            continue;
+        }
+        const attestor = await api.query.attestation.attestors(key.args[0], key.args[1]);
+        if (attestor.isNone) {
+            continue;
+        }
+        const attestorValue = attestor.unwrap();
+        if (attestorValue.stash.toString() == address) {
+            console.log(`Address ${key.args[1]} is an attestor for chain ${chainKey}`);
         }
     }
-
-    const attestor = await api.query.attestation.attestors(chainKey, address);
-    if (attestor.isNone) {
-        console.log(`Address ${address} is not an attestor`);
-        process.exit(0);
-    }
-
-    const status = attestor.unwrap().status;
-    if (status.isActive) {
-        console.log(`Address ${address} status is Active`);
-        process.exit(0);
-    }
-    console.log(`Address ${address} status is Chill`);
     process.exit(0);
 }
