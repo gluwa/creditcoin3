9481ddf50381522331949e4e60d60b1801426796
Alexander Todorov <alex.todorov@gluwa.com>
2025-03-11 10:54:57 +0200

    CSUB-1491: Cleanup and testing for USC (#435)
    * Remove dangling submodule reference
    
    https://github.com/gluwa/creditcoin3-next/pull/401#discussion_r1965100909
    
    * Add bridge-usage-example/ into Dependabot config
    
    * Remove duplicate file, make it into a symlink
    
    * Remove in favor of ../with-hardhat/test/TestERC20.ts
    
    * Remove unnecessary & incorrect `ignition/modules/TestERC20.ts`
    
    - we don't need an ignition module b/c the test file deploys the
      contract before doing anything with it
    
    - also this file was copied incorrectly and it was instantiating the
      wrong contract:
    
      ```
        m.contract('Counter');
      ```
    
    * Update package-lock.json
    
    * Adjust import path from @openzeppelin
    
    - use the path under node_modules/ instead of remapping b/c that's more
      universal and will work well with Hardhat too I believe

--- docs/smart-contract-development/with-hardhat/contracts/TestERC20.sol
+++ docs/smart-contract-development/with-hardhat/contracts/TestERC20.sol
@@ -1,10 +1,1 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract TestERC20 is ERC20 {
-    constructor() ERC20("Burn Test", "TEST") {
-        _mint(msg.sender, 1000000000000000000000);
-    }
-}
+../../../../bridge-usage-example/src/TestERC20.sol
