2c5008f86b3614e1bfc348e392669d684b875e53
Beqa Abuladze <beqaabuladze.00@gmail.com>
2025-03-11 14:15:13 +0400

    feat: set-base-fee pallet

---
+++ pallets/set-base-fee/src/lib.rs
@@ -0,0 +1,96 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::{pallet_prelude::*, traits::EnsureOrigin};
+    use frame_system::pallet_prelude::*;
+    use sp_core::U256;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        /// The origin which may set the base fee.
+        type UpdateOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+
+        /// The default value to use when no value is set.
+        type DefaultBaseFee: Get<U256>;
+    }
+
+    #[pallet::type_value]
+    pub fn DefaultBaseFee<T: Config>() -> U256 {
+        T::DefaultBaseFee::get()
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn base_fee_per_gas)]
+    pub type BaseFeePerGas<T> = StorageValue<_, U256, ValueQuery, DefaultBaseFee<T>>;
+
+    #[pallet::genesis_config]
+    pub struct GenesisConfig<T: Config> {
+        pub base_fee_per_gas: U256,
+        pub _marker: PhantomData<T>,
+    }
+
+    impl<T: Config> GenesisConfig<T> {
+        pub fn new(base_fee_per_gas: U256) -> Self {
+            Self {
+                base_fee_per_gas,
+                _marker: PhantomData,
+            }
+        }
+    }
+
+    impl<T: Config> Default for GenesisConfig<T> {
+        fn default() -> Self {
+            Self {
+                base_fee_per_gas: T::DefaultBaseFee::get(),
+                _marker: PhantomData,
+            }
+        }
+    }
+
+    #[pallet::genesis_build]
+    impl<T: Config> BuildGenesisConfig for GenesisConfig<T> {
+        fn build(&self) {
+            <BaseFeePerGas<T>>::put(self.base_fee_per_gas);
+        }
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Base fee per gas updated. [new_base_fee]
+        BaseFeePerGasUpdated { fee: U256 },
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(T::DbWeight::get().writes(1))]
+        pub fn set_base_fee_per_gas(origin: OriginFor<T>, fee: U256) -> DispatchResult {
+            T::UpdateOrigin::ensure_origin(origin)?;
+
+            BaseFeePerGas::<T>::set(fee);
+
+            Self::deposit_event(Event::BaseFeePerGasUpdated { fee });
+
+            Ok(())
+        }
+    }
+
+    impl<T: Config> Get<U256> for Pallet<T> {
+        fn get() -> U256 {
+            match BaseFeePerGas::<T>::try_get() {
+                Ok(value) => value,
+                Err(_) => T::DefaultBaseFee::get(),
+            }
+        }
+    }
+}
