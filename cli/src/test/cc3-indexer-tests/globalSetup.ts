38e1d2b44ee2707f4a31136a3f3556a6dd6894ea
Alex Todorov <alex.todorov@gluwa.com>
2025-03-25 11:24:23 +0200

    Add test for handleEventProverDeployed()

--- cli/src/test/cc3-indexer-tests/globalSetup.ts
+++ cli/src/test/cc3-indexer-tests/globalSetup.ts
@@ -17,11 +17,26 @@ const createSigner = (who: 'alice' | 'bob' | 'random' | 'sudo'): KeyringPair => 
     }
 };
 
+const evmPrivateKey = (who: 'alice' | 'bob'): string => {
+    // EVM private keys for development accounts are documented at
+    // https://docs.moonbeam.network/builders/get-started/networks/moonbeam-dev/#pre-funded-development-accounts
+    switch (who) {
+        case 'alice': // Alith
+            // 5Fghzk1AJt88PeFEzuRfXzbPchiBbsVGTTXcdx599VdZzkTA 0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac
+            return '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133';
+        case 'bob': // Balthathar
+            return '0x8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b';
+        default:
+            throw new Error(`Unexpected value "${who}"`); // eslint-disable-line
+    }
+};
+
 const setup = () => {
     (global as any).CREDITCOIN_API_URL = 'ws://127.0.0.1:9944';
     (global as any).GRAPHQL_URL = 'http://127.0.0.1:3000';
 
     (global as any).CREDITCOIN_CREATE_SIGNER = createSigner; // eslint-disable-line
+    (global as any).CREDITCOIN_EVM_PRIVATE_KEY = evmPrivateKey; // eslint-disable-line
 };
 
 export default setup;
---
+++ cli/src/test/cc3-indexer-tests/handleEventProverDeployed.test.ts
@@ -0,0 +1,52 @@
+import { WebSocketProvider, ethers } from 'ethers';
+import { chain_Anvil1_Key } from '../blockchain-tests/pallets/supported-chains/consts';
+import { graphQLQuery } from './common';
+
+describe('handleEventProverDeployed)', () => {
+    let alith: any;
+
+    beforeAll(() => {
+        const provider = new WebSocketProvider((global as any).CREDITCOIN_API_URL);
+        const privateKey = (global as any).CREDITCOIN_EVM_PRIVATE_KEY('alice');
+        alith = new ethers.Wallet(privateKey).connect(provider);
+
+        // NOTE: chain starts with prover for Anvil 1 already running
+    }, 30_000);
+
+    describe('when there are provers running', () => {
+        it('graphQL returns known Prover entity', async () => {
+            const response = await graphQLQuery(
+                `query { provers(orderBy: ID_ASC, last: 10) { nodes { id, owner, proceedsAccount, contractAddress, baseCostPerByte, baseFee, chainKey, name }}}`,
+            );
+            expect(response.data.provers.nodes).toBeTruthy();
+            expect(response.data.provers.nodes.length).toBeGreaterThanOrEqual(1);
+
+            for (const node of response.data.provers.nodes) {
+                // these are EVM style addresses
+                expect(node.owner).toEqual(alith.address);
+                expect(node.proceedsAccount).toEqual(alith.address);
+                expect(node.contractAddress.startsWith('0x')).toBeTruthy();
+                expect(BigInt(node.baseCostPerByte)).toBeGreaterThan(0);
+                expect(BigInt(node.baseFee)).toBeGreaterThan(0);
+                // we only have provers for Anvil 1
+                expect(parseInt(node.chainKey, 10)).toEqual(chain_Anvil1_Key);
+                // this is hard-coded in ci.yaml
+                expect(node.name).toEqual('Prover-1-for-Alice');
+
+                // query each node individually to cover this endpoint too
+                const response2 = await graphQLQuery(
+                    `query { prover(id: "${node.id}") { id, owner, proceedsAccount, contractAddress, baseCostPerByte, baseFee, chainKey, name }}`,
+                );
+                expect(response2.data.prover).toBeTruthy();
+                expect(response2.data.prover.id).toEqual(node.id);
+                expect(response2.data.prover.owner).toEqual(node.owner);
+                expect(response2.data.prover.proceedsAccount).toEqual(node.proceedsAccount);
+                expect(response2.data.prover.contractAddress).toEqual(node.contractAddress);
+                expect(response2.data.prover.baseCostPerByte).toEqual(node.baseCostPerByte);
+                expect(response2.data.prover.baseFee).toEqual(node.baseFee);
+                expect(response2.data.prover.chainKey).toEqual(node.chainKey);
+                expect(response2.data.prover.name).toEqual(node.name);
+            }
+        });
+    });
+});
