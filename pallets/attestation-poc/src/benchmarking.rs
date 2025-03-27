ca7ed76b91d75df9964f4042c0d04897535606a5
rustlang-dev <rust.lang.developer@gmail.com>
2025-03-27 10:19:25 +0000

    remove unused import

--- pallets/attestation-poc/src/benchmarking.rs
+++ pallets/attestation-poc/src/benchmarking.rs
@@ -11,8 +11,8 @@ use sp_std::vec;
 use sp_std::vec::Vec;
 
 use attestor_primitives::{
-    Attestation as AttestationPrimitive, AttestationCheckpoint, BlsPublicKey, BlsSignature,
-    ChainAttestationIntervalType, ChainKey, SignedAttestation,
+    Attestation as AttestationPrimitive, BlsPublicKey, BlsSignature, ChainAttestationIntervalType,
+    ChainKey, SignedAttestation,
 };
 
 const DEV_CHAIN_KEY: ChainKey = 1;
