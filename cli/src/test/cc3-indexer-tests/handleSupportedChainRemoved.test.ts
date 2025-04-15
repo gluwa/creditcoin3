4a634283ede3bfcb0f703c8c02d143e73c46764a
Alex Todorov <alex.todorov@gluwa.com>
2025-04-15 12:40:03 +0300

    Add tests for cc3-indexer::handleSupportedChainRemoved()

---
+++ cli/src/test/cc3-indexer-tests/handleSupportedChainRemoved.test.ts
@@ -0,0 +1,97 @@
+import { newApi, ApiPromise, KeyringPair } from '../../lib';
+import { getChainStatus } from '../../lib/chain/status';
+import { forElapsedBlocks } from '../utils';
+import { graphQLQuery } from './common';
+
+describe('handleSupportedChainRemoved()', () => {
+    let api: ApiPromise;
+    let root: KeyringPair;
+    let startingBlock: number;
+    // unique integer to serve as chain id during testing
+    const newChainId = BigInt(Date.now());
+    const newChainName = `Test Chain ${newChainId}`;
+    let newChainKey = 0;
+
+    beforeAll(async () => {
+        ({ api } = await newApi((global as any).CREDITCOIN_API_URL));
+        root = (global as any).CREDITCOIN_CREATE_SIGNER('sudo');
+
+        await api.tx.sudo.sudo(api.tx.supportedChains.registerChain(newChainId, newChainName)).signAndSend(root);
+        await forElapsedBlocks(api, { minBlocks: 1 });
+
+        // will fail if the query returns None
+        newChainKey = (await api.query.supportedChains.chainIdAndNameToUniqKey(newChainId, newChainName))
+            .unwrap()
+            .toNumber();
+        expect(newChainKey).toBeGreaterThan(0);
+
+        // there should be a SupportedChain entity for this new chain
+        await forElapsedBlocks(api, { minBlocks: 3 });
+        const response = await graphQLQuery(
+            `query {
+                supportedChains(
+                    filter: { chainKey: { equalTo: ${newChainKey} }},
+                    last: 1,
+                ) { nodes { id, at, chainKey, chainName, chainId }}}`,
+        );
+        expect(response.data.supportedChains.nodes).toBeTruthy();
+        expect(response.data.supportedChains.nodes.length).toEqual(1);
+
+        for (const node of response.data.supportedChains.nodes) {
+            expect(node.id).toBeTruthy();
+            // note: inspecting only last record
+            expect(node.chainKey).toEqual(newChainKey);
+            expect(node.chainName).toEqual(newChainName);
+            expect(BigInt(node.chainId)).toEqual(newChainId);
+        }
+    }, 60_000);
+
+    afterAll(async () => {
+        await api.disconnect();
+    });
+
+    describe('when a supported chain is removed', () => {
+        beforeAll(async () => {
+            startingBlock = (await getChainStatus(api)).bestNumber;
+            expect(startingBlock).toBeGreaterThan(0);
+
+            await api.tx.sudo.sudo(api.tx.supportedChains.removeChain(newChainKey, true)).signAndSend(root);
+
+            await forElapsedBlocks(api, { minBlocks: 3 });
+        }, 60_000);
+
+        it('graphQL returns known ChainRemoved entity', async () => {
+            const response = await graphQLQuery(
+                `query {
+                    chainRemoveds(
+                        filter: { chainKey: { equalTo: ${newChainKey} }},
+                        last: 1,
+                    ) { nodes { id, at, chainKey, chainName, chainId, whoId }}}`,
+            );
+            expect(response.data.chainRemoveds.nodes).toBeTruthy();
+            expect(response.data.chainRemoveds.nodes.length).toEqual(1);
+
+            for (const node of response.data.chainRemoveds.nodes) {
+                expect(node.id).toBeTruthy();
+                // note: inspecting only last record
+                expect(node.at).toBeGreaterThanOrEqual(startingBlock);
+                expect(node.chainKey).toEqual(newChainKey);
+                expect(node.chainName).toEqual(newChainName);
+                expect(BigInt(node.chainId)).toEqual(newChainId);
+                expect(node.whoId).toEqual(root.address);
+            }
+        });
+
+        it('known SupportedChain entity should be removed', async () => {
+            const response = await graphQLQuery(
+                `query {
+                    supportedChains(
+                        filter: { chainKey: { equalTo: ${newChainKey} }},
+                        last: 1,
+                    ) { nodes { id, at, chainKey, chainName, chainId }}}`,
+            );
+            expect(response.data.supportedChains.nodes).toBeTruthy();
+            expect(response.data.supportedChains.nodes.length).toEqual(0);
+        });
+    });
+});
