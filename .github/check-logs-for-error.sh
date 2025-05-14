e3ee62fd8d92552c258897c175f9d87a9c68a295
Alex Todorov <alex.todorov@gluwa.com>
2025-05-14 10:38:14 +0300

    Check attestor logs for errors at the end of test cycle
    also update .github/check-logs-for-error.sh to iterate over the target
    specified as argument so that we can also pass directories.

--- .github/check-logs-for-error.sh
+++ .github/check-logs-for-error.sh
@@ -2,23 +2,31 @@
 
 set -euo pipefail
 
-LOG_FILE="$1"
-echo "INFO: log file is '$LOG_FILE'"
+TARGET_FILE="$1"
+echo "INFO: target file is '$TARGET_FILE'"
 
-if [ -z "$LOG_FILE" ]; then
-    echo "ERROR: no log file specified"
+if [ -z "$TARGET_FILE" ]; then
+    echo "ERROR: no target file specified"
     exit 1
 fi
 
-# check for errors in the logs
-set +e
-ERR_COUNT=$(grep -c -i "ERROR:" "$LOG_FILE")
-set -e
-if [ "$ERR_COUNT" -gt 0 ]; then
-    echo "FAIL: found $ERR_COUNT errors in $LOG_FILE"
-    exit "$ERR_COUNT"
-else
-    echo "PASS: no errors found in $LOG_FILE"
-fi
+# shellcheck disable=SC2044
+for LOG_FILE in $(find "$TARGET_FILE" -type f ); do
+    echo "INFO: inspecting file '$LOG_FILE'"
+
+    # check for errors in the logs
+    set +e
+    ERR_COUNT=$(grep -c -i "ERROR:" "$LOG_FILE")
+    set -e
+    if [[ "$ERR_COUNT" -gt 0 ]]; then
+        echo "FAIL: found $ERR_COUNT errors in $LOG_FILE"
+        echo "======"
+        grep -i "ERROR:" "$LOG_FILE"
+        echo "======"
+        exit "$ERR_COUNT"
+    else
+        echo "PASS: no errors found in $LOG_FILE"
+    fi
+done
 
 exit 0
