0cdcd1a8c9e1f1ab61393311a5e2755677a60bef
BradleyOlson64 <lotrftw9@gmail.com>
2025-05-15 14:55:42 -0700

    Fixed hex value

--- pallets/prover/src/test_helpers.rs
+++ pallets/prover/src/test_helpers.rs
@@ -3,7 +3,7 @@ use sp_core::H256;
 use sp_std::vec::Vec;
 
 pub const PROOF_EXAMPLE_DIGEST_HEX: &str =
-    "0x0356f5b61833ff38a0322cfea30f745d0948a7b1baaff455b7ea34e41812f560";
+    "0356f5b61833ff38a0322cfea30f745d0948a7b1baaff455b7ea34e41812f560";
 
 pub fn create_dummy_attestation<AccountId>(
     chain_key: ChainKey,
