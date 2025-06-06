6381abd59db4d620d3c67a18f0c8a1781a9ba7cb
Alex Todorov <alex.todorov@gluwa.com>
2025-06-06 16:10:48 +0300

    Add test for attestor unregister when still active

--- cli/src/test/blockchain-tests/pallets/supported-chains/consts.ts
+++ cli/src/test/blockchain-tests/pallets/supported-chains/consts.ts
@@ -1,5 +1,7 @@
 // node/src/chain_spec.rs
 export const chain_Anvil1_Key = 2;
+export const chain_Anvil1_Url = 'http://localhost:8141';
+
 export const chain_Anvil2_Key = 4;
 
 export const chain_Anvil3_Id = 31339;
