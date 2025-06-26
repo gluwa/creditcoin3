c5b515a26b744bf3cd91514d660a2ca634b750c0
marshacb <cameron.marshall12@gmail.com>
2025-06-26 15:57:45 -0400

    fix: show-status doesn't check required argument chain

--- cli/src/commands/attestor/showAttestorStatus.ts
+++ cli/src/commands/attestor/showAttestorStatus.ts
@@ -1,12 +1,12 @@
 import { Command, OptionValues } from 'commander';
 import { newApi } from '../../lib';
-import { substrateAddressOption } from '../options';
+import { substrateAddressOption, chainKeyOption } from '../options';
 
 export function makeShowAttestorStatusCommand() {
     const cmd = new Command('show-status');
     cmd.description('Show attestor status for a given address and chain key');
     cmd.addOption(substrateAddressOption.makeOptionMandatory());
-    cmd.option('-c, --chain [chain]', 'Specify chain key to show attestor status for');
+    cmd.addOption(chainKeyOption.makeOptionMandatory());
     cmd.action(showAttestorStatus);
     return cmd;
 }
