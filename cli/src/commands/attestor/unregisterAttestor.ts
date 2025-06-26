b8ffcfd8d80b28db5b7a65d723c5dc3238db41f8
marshacb <cameron.marshall12@gmail.com>
2025-06-26 14:48:34 -0400

    update unregister option var names

--- cli/src/commands/attestor/unregisterAttestor.ts
+++ cli/src/commands/attestor/unregisterAttestor.ts
@@ -2,13 +2,13 @@ import { Command, OptionValues } from 'commander';
 import { newApi } from '../../lib';
 import { requireKeyringHasSufficientFunds, signSendAndWatchCcKeyring } from '../../lib/tx';
 import { initKeyring } from '../../lib/account/keyring';
-import { proxyForOption, attestorOption, chainKeyOption } from '../options';
+import { proxyForOption, unregisterAttestorCmdAttestorOption, unregisterAttestorCmdChainKeyOption } from '../options';
 
 export function makeUnregisterAttestorCommand() {
     const cmd = new Command('unregister');
     cmd.description('Unregister attestor and unbond funds from a stash account');
-    cmd.addOption(attestorOption.makeOptionMandatory());
-    cmd.addOption(chainKeyOption.makeOptionMandatory());
+    cmd.addOption(unregisterAttestorCmdAttestorOption.makeOptionMandatory());
+    cmd.addOption(unregisterAttestorCmdChainKeyOption.makeOptionMandatory());
     cmd.addOption(proxyForOption);
     cmd.action(unregisterAttestorAction);
     return cmd;
