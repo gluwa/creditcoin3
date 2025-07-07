3efa19ed2cfd623f74db3857d330bcc190d81dbd
Alex Todorov <alex.todorov@gluwa.com>
2025-07-07 15:32:54 +0300

    Compare current Prover.sol against artifacts from latest release
    NOTE: show diff in reverse in order to produce the same output when
    comparing Current vs. Released (the actual comparison is
    Released vs. Current due to technical implementation).

--- .github/extract-release-tag.sh
+++ .github/extract-release-tag.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 ### find the latest release for testnet or mainnet
+OWNER_REPO_SLUG="${GITHUB_REPOSITORY:-gluwa/creditcoin3}"
 GREP_FOR="$1"
-RELEASE_TAG=$(curl --silent https://api.github.com/repos/gluwa/creditcoin3/releases | jq -r ".[].tag_name" | grep "$GREP_FOR" | head -n1)
+RELEASE_TAG=$(curl --silent --header "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER_REPO_SLUG/releases" | jq -r ".[].tag_name" | grep "$GREP_FOR" | head -n1)
 
 echo "$RELEASE_TAG"
