e27ac3fc8098e61401097c6e1725af59fb3f1c01
Beqa Abuladze <beqaabuladze.00@gmail.com>
2025-07-08 08:44:19 +0400

    chore: wait for anvil2 attestors

--- .github/wait-for-attestors.sh
+++ .github/wait-for-attestors.sh
@@ -6,11 +6,21 @@ TARGET_URL=${1:-http://127.0.0.1:9944}
 COUNTER=0
 RESULT="null"
 
+# Choose the correct storage key based on the URL
+# this is attestation.activeAttestors(DEV_CHAIN_ID) via the Chain state -> Storage tab in the Substrate Portal
+CHAIN_KEY_2="0x6310fed47319b658f9b8b2504e0d72ec605e795422de90908f14285054a6764bfc069c24352798859c017ce862813d3b0200000000000000"
+CHAIN_KEY_4="0x6310fed47319b658f9b8b2504e0d72ec605e795422de90908f14285054a6764ba4f5ced6668957bb2a9a954e7e50f5b50400000000000000"
+
+if [[ "$TARGET_URL" == "http://127.0.0.1:9944" ]]; then
+    STORAGE_KEY="$CHAIN_KEY_2"
+else
+    STORAGE_KEY="$CHAIN_KEY_4"
+fi
+
 # make sure there is a node running at TARGET_URL
 while [[ "$RESULT" == "null" && $COUNTER -lt 15 ]]; do
-    # this is attestation.activeAttestors(DEV_CHAIN_ID) via the Chain state -> Storage tab in the Substrate Portal
     RESULT=$(curl -H 'Content-Type: application/json' \
-                -d '{"id":"1", "jsonrpc":"2.0", "method": "state_getStorage", "params":["0x6310fed47319b658f9b8b2504e0d72ec605e795422de90908f14285054a6764bfc069c24352798859c017ce862813d3b0200000000000000"]}' \
+                -d "{\"id\":\"1\", \"jsonrpc\":\"2.0\", \"method\": \"state_getStorage\", \"params\":[\"$STORAGE_KEY\"]}" \
                 "$TARGET_URL" | jq -r .result
         )
 
