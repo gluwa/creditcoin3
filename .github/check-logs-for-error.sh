14553f688c77953d92e1763b4e540a4201377542
Alex Todorov <alex.todorov@gluwa.com>
2025-09-09 16:48:35 +0300

    Ignore a DEBUG error message seen in #857
    the message is:
    DEBUG tokio-runtime-worker jsonrpsee-server: WS send error: connection closed
    
    https://gluwa.slack.com/archives/D06QAG5D6UU/p1757424176585559

--- .github/check-logs-for-error.sh
+++ .github/check-logs-for-error.sh
@@ -17,12 +17,12 @@ for LOG_FILE in $(find "$TARGET_FILE" -type f ); do
     # check for errors in creditcoin3-node logs
     # NOTICE: ignoring libp2p connection errors
     set +e
-    ERR_COUNT=$(grep -i "ERROR:" "$LOG_FILE" | grep -v "libp2p" | grep -c -i "ERROR:")
+    ERR_COUNT=$(grep -i "ERROR:" "$LOG_FILE" | grep -v "libp2p" | grep -v "DEBUG tokio-runtime-worker jsonrpsee-server: WS send error: connection closed" | grep -c -i "ERROR:")
     set -e
     if [[ "$ERR_COUNT" -gt 0 ]]; then
         echo "FAIL: found $ERR_COUNT errors in $LOG_FILE"
         echo "======"
-        grep -i "ERROR:" "$LOG_FILE" | grep -v "libp2p"
+        grep -i "ERROR:" "$LOG_FILE" | grep -v "libp2p" | grep -v "DEBUG tokio-runtime-worker jsonrpsee-server: WS send error: connection closed"
         echo "======"
         exit "$ERR_COUNT"
     else
