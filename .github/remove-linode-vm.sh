300e1b01279ea33708ffda4592871104b831234a
Alex Todorov <alex.todorov@gluwa.com>
2025-09-09 14:46:51 +0300

    If Linode VM has a keep_until_* tag take it into account when removing

--- .github/remove-linode-vm.sh
+++ .github/remove-linode-vm.sh
@@ -15,8 +15,26 @@ if [ -z "$LC_RUNNER_VM_NAME" ]; then
     # dump JSON for debugging purposes
     linode-cli linodes list --json | jq -r
     for VM_ID in $(linode-cli linodes list --json | jq ".[] | select(.created <= \"$THRESHOLD\")" | jq -r '.id'); do
-        echo "INFO: going to remove expired VM $VM_ID"
-        linode-cli linodes delete "$VM_ID"
+        TAGS=$(linode-cli linodes list --id "$VM_ID" --json | jq -r .tags[])
+
+        if [ -z "$TAGS" ]; then
+            echo "INFO: No tags specified. Going to remove expired VM $VM_ID"
+            linode-cli linodes delete "$VM_ID"
+        else
+            for TAG in $TAGS; do
+                if [[ "$TAG" =~ ^keep_until_.* ]]; then
+                    KEEP_UNTIL=$(echo -n "$TAG" | sed s/keep_until_//)
+                    NOW=$(date --utc "+%Y-%m-%dT%H:%M:%S")
+
+                    if [[ "$KEEP_UNTIL" < "$NOW" ]]; then
+                        echo "INFO: $NOW is past $KEEP_UNTIL. Going to remove expired VM $VM_ID"
+                        linode-cli linodes delete "$VM_ID"
+                    else
+                        echo "INFO: $NOW is before $KEEP_UNTIL. NOT removing VM $VM_ID"
+                    fi
+                fi
+            done
+        fi
     done
 else
     VM_ID=$(linode-cli linodes list --json --label "$LC_RUNNER_VM_NAME" | jq -r '.[0].id')
