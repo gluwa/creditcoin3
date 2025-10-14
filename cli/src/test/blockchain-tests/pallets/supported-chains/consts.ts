95db8b7f7e517e02f7949c40f091b1000022d195
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2025-10-14 13:31:06 +0200

    fix: revert const in test

--- cli/src/test/blockchain-tests/pallets/supported-chains/consts.ts
+++ cli/src/test/blockchain-tests/pallets/supported-chains/consts.ts
@@ -1,6 +1,6 @@
 // node/src/chain_spec.rs
 export const chain_Anvil1_Key = 2;
-export const chain_Anvil1_Url = 'ws://localhost:8545';
+export const chain_Anvil1_Url = 'ws://localhost:8141';
 
 export const chain_Anvil2_Key = 4;
 export const chain_Anvil2_Url = 'ws://localhost:8242';
