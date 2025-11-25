3da60fc10575380b7cb6c01aace66008df9c07ce
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2025-11-25 16:41:59 +0100

    chore: update common eth build

--- common/eth/build.rs
+++ common/eth/build.rs
@@ -1,4 +1,4 @@
 fn main() {
     // Path is relative to the crate root
-    println!("cargo::rerun-if-changed=contracts/native_query_verifier.json");
+    println!("cargo::rerun-if-changed=contracts/nativeQueryVerifier.abi.json");
 }
