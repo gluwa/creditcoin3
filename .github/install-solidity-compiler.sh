a484f8eec28c381113cfa60c7c4a025d7ac6e824
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2025-12-11 17:14:48 +0100

    fix(docker): use snap install

--- .github/install-solidity-compiler.sh
+++ .github/install-solidity-compiler.sh
@@ -2,8 +2,10 @@
 
 # NOTE: needs to be executed via sudo
 
-set -euo pipefail
+# set -euo pipefail
 
-curl -L -H "Cache-Control: no-cache" https://github.com/argotorg/solidity/releases/download/v0.8.31/solc-static-linux > /usr/bin/solc
+# curl -L -H "Cache-Control: no-cache" https://github.com/argotorg/solidity/releases/download/v0.8.31/solc-static-linux > /usr/bin/solc
 
-chmod a+x /usr/bin/solc
+# chmod a+x /usr/bin/solc
+
+sudo snap install solc
