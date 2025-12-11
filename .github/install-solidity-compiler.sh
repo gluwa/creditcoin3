f8f7dc3ac83f9bc0fabc54cbe5e0ed0576f35b9f
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2025-12-11 17:05:31 +0100

    fix(docker): use latest solidity bin with updated repo

--- .github/install-solidity-compiler.sh
+++ .github/install-solidity-compiler.sh
@@ -4,6 +4,6 @@
 
 set -euo pipefail
 
-curl -L https://github.com/ethereum/solidity/releases/download/v0.8.29/solc-static-linux > /usr/bin/solc
+curl -L https://github.com/argotorg/solidity/releases/download/v0.8.31/solc-static-linux > /usr/bin/solc
 
 chmod a+x /usr/bin/solc
