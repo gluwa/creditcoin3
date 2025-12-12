aab775e9aa016d4feb0d57dce01335ac4b0374ce
Beqa Abuladze <beqaabuladze.00@gmail.com>
2025-12-12 23:00:38 +0400

    chore: add unhappy path for the bn254 precompile test and fmt

--- docs/smart-contract-development/with-hardhat/test/Counter.ts
+++ docs/smart-contract-development/with-hardhat/test/Counter.ts
@@ -1,25 +1,24 @@
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
-import { Contract } from 'ethers';
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Contract } from "ethers";
 
-describe('Counter contract', function () {
-    let counter: Contract;
+describe("Counter contract", function () {
+  let counter: Contract;
 
-    beforeEach(async function () {
-        // Deploy the Counter contract before the tests
-        const CounterFactory = await ethers.getContractFactory("Counter");
-        counter = await CounterFactory.deploy();
-        await counter.waitForDeployment();
-    });
+  beforeEach(async function () {
+    // Deploy the Counter contract before the tests
+    const CounterFactory = await ethers.getContractFactory("Counter");
+    counter = await CounterFactory.deploy();
+    await counter.waitForDeployment();
+  });
 
-    it("Should be deployed with the initial value 0", async function ()
-    {
-        const count = await counter.getCount();
-        expect(count).to.equal(0);
-    });
+  it("Should be deployed with the initial value 0", async function () {
+    const count = await counter.getCount();
+    expect(count).to.equal(0);
+  });
 
-    it('Should increment count by 1', async function () {
-        await counter.incrementCounter();
-        expect(await counter.getCount()).to.equal(1);
-    });
+  it("Should increment count by 1", async function () {
+    await counter.incrementCounter();
+    expect(await counter.getCount()).to.equal(1);
+  });
 });
