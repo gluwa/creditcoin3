444df4777b7ea1d394a420ba3dc38c72b8fdd2dd
Beqa Abuladze <beqaabuladze.00@gmail.com>
2026-01-23 16:26:09 +0400

    chore: fix deno lint

--- scripts/traffic-simulator/src/config.ts
+++ scripts/traffic-simulator/src/config.ts
@@ -5,7 +5,7 @@
  * CLI arguments take precedence over environment variables.
  */
 
-import { parseArgs } from "jsr:@std/cli/parse-args";
+import { parseArgs } from "jsr:@std/cli@^1.0.0/parse-args";
 import type { SimulatorConfig } from "./types.ts";
 
 /**
