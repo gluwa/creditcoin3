698626071b2f8ff642458ff4023f7a7f337a8ca0
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-03-06 13:24:50 +0100

    feat(stream): err

--- common/streams/eth/src/error.rs
+++ common/streams/eth/src/error.rs
@@ -1,13 +1,17 @@
 #[derive(Debug)]
 pub enum Error {
     Client(eth::Error),
-    StreamEnd,
     Task(tokio::task::JoinError),
+    StreamEnd,
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        match self {
+            Self::Client(err) => write!(f, "{err}"),
+            Self::Task(err) => write!(f, "{err}"),
+            Self::StreamEnd => write!(f, "Unexpected end of stream"),
+        }
     }
 }
 impl std::error::Error for Error {}
