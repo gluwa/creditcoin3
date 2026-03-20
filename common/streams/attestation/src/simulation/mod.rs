64cb3cc75d77808c6b47ea05d12985447a62ea98
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-03-20 12:55:01 +0100

    feat(simulation): incrased simulation test count

--- common/streams/attestation/src/simulation/mod.rs
+++ common/streams/attestation/src/simulation/mod.rs
@@ -3,6 +3,7 @@ mod core;
 use proptest::prelude::*;
 
 proptest! {
+    #![proptest_config(ProptestConfig::with_cases(1_000))]
     #[test]
     fn simulate(sim in core::simulation("ws://localhost:9944".parse().unwrap())) {
         sim.run()
