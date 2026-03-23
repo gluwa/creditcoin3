a9f4c54d71f22b34dab8233c176c81e9aefd21a1
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-03-23 13:14:24 +0100

    docs(simulation)

--- common/streams/attestation/src/simulation/mod.rs
+++ common/streams/attestation/src/simulation/mod.rs
@@ -5,6 +5,9 @@ use proptest::prelude::*;
 proptest! {
     #![proptest_config(ProptestConfig::with_cases(1_000))]
     #[test]
+    /// Simulates 1000 pseudo-random testing scenarios for the attestation stream to pass.
+    /// Historical fail cases stored inside `proptest-regressions` are also run to avoid
+    /// regressions.
     fn simulate(sim in core::simulation("ws://localhost:9944".parse().unwrap())) {
         sim.run()
     }
