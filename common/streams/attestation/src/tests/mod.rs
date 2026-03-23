4c00d442fa9d722ef9fcf8aa223c8de9c194fb8a
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-03-23 12:37:42 +0100

    test(simulation): ignore past finalizations

--- common/streams/attestation/src/tests/mod.rs
+++ common/streams/attestation/src/tests/mod.rs
@@ -34,7 +34,7 @@ async fn attestation_ready_simple(
 /// invalid as the end of the range is used is assumed to be strictly greater than its start.
 #[rstest::rstest]
 #[tokio::test]
-async fn attestation_finalize_sets_correct_range(
+async fn attestation_finalization_sets_correct_range(
     #[future]
     #[with(0, nonzero!(1), nonzero!(1))]
     attestations: (mock::RootSender, mock::TipSender, crate::StreamAttestation),
@@ -68,11 +68,64 @@ async fn attestation_finalize_sets_correct_range(
     assert_eq!(stream_attestation.computed, 1..=2);
 }
 
+#[rstest::rstest]
+#[tokio::test]
+async fn attestation_finalization_ignore_past_attestation(
+    #[future]
+    #[with(0, nonzero!(1), nonzero!(2))]
+    attestations: (mock::RootSender, mock::TipSender, crate::StreamAttestation),
+) {
+    let (mut roots, mut tip, mut stream_attestation) = attestations.await;
+
+    tip.send_ready().await; // 0
+    tip.send_ready().await; // 1
+    tip.send_ready().await; // 2
+
+    assert!(poll!(stream_attestation).is_pending());
+
+    roots.send_ready().await; // 0 - skipped, 0 is always ignored
+    roots.send_ready().await; // 1
+    roots.send_ready().await; // 2
+
+    let std::task::Poll::Ready(Some(Ok(attestation))) = poll!(stream_attestation) else {
+        panic!("Failed to generate attestation");
+    };
+
+    assert_eq!(attestation.header_number(), 2);
+    assert_eq!(attestation.continuity_proof.len(), 1);
+
+    // Marks latest root height as finalized
+    stream_attestation.note_attestation_finalization(stream_util::AttestationInfo {
+        height: 2,
+        ..Default::default()
+    });
+
+    assert_eq!(stream_attestation.computed, 2..=2);
+
+    // Past finalizations should not update the state of the stream
+    stream_attestation.note_attestation_finalization(stream_util::AttestationInfo {
+        height: 1,
+        ..Default::default()
+    });
+
+    assert_eq!(stream_attestation.computed, 2..=2);
+
+    tip.send_ready().await; // 3
+    roots.send_ready().await; // 3
+
+    let std::task::Poll::Ready(Some(Ok(attestation))) = poll!(stream_attestation) else {
+        panic!("Failed to generate attestation");
+    };
+
+    assert_eq!(attestation.header_number(), 3);
+    assert!(attestation.continuity_proof.is_empty());
+}
+
 /// Attestation stream cache size should grow up to `max_catchup` + 1 in order to allow the last
 /// block in the cache to be a multiple of the `max_catchup`.
 #[rstest::rstest]
 #[tokio::test]
-async fn cache_size(
+async fn max_cache_size(
     #[future]
     #[with(2, nonzero!(1), nonzero!(1))]
     attestations: (mock::RootSender, mock::TipSender, crate::StreamAttestation),
