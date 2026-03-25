d28c78f78539e513333e3df5d201fd0fce1501a8
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-03-25 10:54:01 +0100

    ci(simulation)

--- common/streams/attestation/src/tests/mod.rs
+++ common/streams/attestation/src/tests/mod.rs
@@ -11,7 +11,6 @@ use fixtures::*;
 /// Polling the attestation stream should yield available attestations.
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn attestation_ready_simple(
     #[future]
     #[with(0, nonzero!(1), nonzero!(1))]
@@ -37,7 +36,6 @@ async fn attestation_ready_simple(
 /// invalid as the end of the range is used is assumed to be strictly greater than its start.
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn attestation_finalization_sets_correct_range(
     #[future]
     #[with(0, nonzero!(1), nonzero!(1))]
@@ -74,7 +72,6 @@ async fn attestation_finalization_sets_correct_range(
 
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn attestation_finalization_ignore_past_attestation(
     #[future]
     #[with(0, nonzero!(1), nonzero!(2))]
@@ -130,7 +127,6 @@ async fn attestation_finalization_ignore_past_attestation(
 /// block in the cache to be a multiple of the `max_catchup`.
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn max_cache_size(
     #[future]
     #[with(2, nonzero!(1), nonzero!(1))]
@@ -163,7 +159,6 @@ async fn max_cache_size(
 /// the chain. Outdated data returned by the root stream needs to be skipped if this is the case.
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn skip_behind_finality(
     #[future]
     #[with(0, nonzero!(1), nonzero!(2))]
@@ -201,7 +196,6 @@ async fn skip_behind_finality(
 /// should keep growing to encompass new blocks.
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn continuity_proofs_should_grow(
     #[future]
     #[with(0, nonzero!(1), nonzero!(1))]
@@ -255,7 +249,6 @@ async fn continuity_proofs_should_grow(
 // has changed.
 #[rstest::rstest]
 #[tokio::test]
-#[ignore]
 async fn regenerate_attestations(
     #[future]
     #[with(0, nonzero!(1), nonzero!(2))]
