cef053dff6a378621a9169a9f1682e7e19e807b4
Alex Todorov <alex.todorov@gluwa.com>
2026-05-04 11:37:32 +0300

    Remove unused option --ecdsa and its supporting functions

--- cli/src/lib/account/keyring.ts
+++ cli/src/lib/account/keyring.ts
@@ -16,11 +16,6 @@ export function initKeyringPair(seed: string) {
     const pair = keyring.addFromUri(`${seed}`);
     return pair;
 }
-export function initECDSAKeyringPairFromPK(pk: string) {
-    const keyring = new Keyring({ type: 'ecdsa' });
-    const pair = keyring.addFromUri(`${pk}`);
-    return pair;
-}
 
 export function initEthKeyringPair(seed: string, accIndex = 0) {
     const keyring = new Keyring({ type: 'ethereum' });
@@ -35,9 +30,7 @@ export async function initKeyringFromEnvOrPrompt(
 ): Promise<KeyringPair> {
     // General configs
     const interactive = setInteractivity(options);
-    const inputName = options.useEcdsa ? 'private key' : 'seed phrase';
-    const validateInput = options.useEcdsa ? () => true : mnemonicValidate;
-    const generateKeyring = options.useEcdsa ? initECDSAKeyringPairFromPK : initKeyringPair;
+    const inputName = 'seed phrase';
 
     if (!interactive && !process.env[envVar]) {
         throw new Error(
@@ -47,8 +40,8 @@ export async function initKeyringFromEnvOrPrompt(
 
     if (typeof process.env[envVar] === 'string') {
         const input = getStringFromEnvVar(process.env[envVar]);
-        if (validateInput(input)) {
-            return generateKeyring(input);
+        if (mnemonicValidate(input)) {
+            return initKeyringPair(input);
         } else {
             throw new Error(`Error: Seed phrase provided in environment variable ${envVar} is invalid.`);
         }
@@ -58,13 +51,13 @@ export async function initKeyringFromEnvOrPrompt(
                 type: 'password',
                 name: 'seed',
                 message: `Specify a ${inputName} for the ${accountRole} account`,
-                validate: (input) => validateInput(input as string),
+                validate: (input) => mnemonicValidate(input as string),
             },
         ]);
         // If SIGTERM is issued while prompting, it will log a bogus address anyways and exit without error.
         // To avoid this, we check if prompt was successful, before returning.
         if (promptResult.seed) {
-            return generateKeyring(promptResult.seed as string);
+            return initKeyringPair(promptResult.seed as string);
         }
     }
     throw new Error(`Error: Could not retrieve ${inputName}`);
