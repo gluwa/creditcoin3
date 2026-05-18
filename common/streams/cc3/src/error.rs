e06b120ed31b1254d27a7fc9710bb5393bdc5f7d
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-05-18 12:12:52 +0200

    refactor(cc3): fallible cc3 stream reconnection

--- common/streams/cc3/src/error.rs
+++ common/streams/cc3/src/error.rs
@@ -1,16 +1,14 @@
 #[derive(Debug)]
 pub enum Error {
-    Subxt(subxt::Error),
+    Client(cc_client::Error),
     EndOfStream,
-    BlockHash(attestor_primitives::Height),
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Subxt(err) => write!(f, "{err}"),
+            Self::Client(err) => write!(f, "{err}"),
             Self::EndOfStream => write!(f, "Unexpected end of stream"),
-            Self::BlockHash(n) => write!(f, "Failed to retrieve hash for block {n}"),
         }
     }
 }
