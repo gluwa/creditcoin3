e66341eaa4a7450d6fe438656a88fe5bfdfd6bad
creditcoinprotoclaw <devops+creditcoinopenclaw@gluwa.com>
2026-07-02 09:16:07 +0000

    Bump version to 3.128.0 (mandatory runtime upgrade)

--- runtime/src/version.rs
+++ runtime/src/version.rs
@@ -6,7 +6,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("creditcoin3"),
     impl_name: Cow::Borrowed("creditcoin3"),
     authoring_version: 3,
-    spec_version: 127,
+    spec_version: 128,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
