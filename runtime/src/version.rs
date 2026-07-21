24ba30c069f012832c8190e6938d86c8dfa3699f
creditcoinprotoclaw <devops+creditcoinopenclaw@gluwa.com>
2026-07-21 13:30:23 +0000

    DEBUG: bump version to 3.131.0 to trigger runtime upgrade

--- runtime/src/version.rs
+++ runtime/src/version.rs
@@ -6,7 +6,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("creditcoin3"),
     impl_name: Cow::Borrowed("creditcoin3"),
     authoring_version: 3,
-    spec_version: 130,
+    spec_version: 131,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
